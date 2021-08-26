60f1a71d3d0b0d84ced03caf2fc4239cc04651d0
Senna Ohmura <29295263+Senna46@users.noreply.github.com>
2021-08-26 10:32:59 +0900

    feat: angular project

---
+++ electoric-power-exchange/src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AiueoComponent } from './aiueo/aiueo.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [{path:'', component: HomeComponent},
+ {path:'aiueo', component: AiueoComponent}];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
