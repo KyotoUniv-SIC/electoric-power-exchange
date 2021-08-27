867fc1a04524e3b1906965389abfafc3225a86df
Kimura Yu <33382781+KimuraYu45z@users.noreply.github.com>
2021-08-27 10:57:38 +0900

    feat: scaffold

--- projects/electoric-power-exchange/karma.conf.js
+++ projects/electoric-power-exchange/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/electoric-power-exchange'),
+      dir: require('path').join(__dirname, '../../coverage/electoric-power-exchange'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- projects/electoric-power-exchange/src/app/app.module.ts
+++ projects/electoric-power-exchange/src/app/app.module.ts
@@ -3,14 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { AiueoComponent } from './aiueo/aiueo.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    AiueoComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
