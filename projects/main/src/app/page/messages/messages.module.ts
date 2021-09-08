3ab26bdb13d659862dbb3d4988860f047c38d8c6
Senna Ohmura <29295263+Senna46@users.noreply.github.com>
2021-09-08 16:41:54 +0900

    feat: messages

--- projects/main/src/app/page/messages/messages.module.ts
+++ projects/main/src/app/page/messages/messages.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MessagesRoutingModule } from './messages-routing.module';
+import { MessagesComponent } from './messages.component';
+import { MessageComponent } from './message/message.component';
+import { MessagesModule } from '../../view/messages/messages.module';
+import { MessageModule } from '../../view/messages/message/message.module';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MessagesComponent,
+    MessageComponent
+  ],
   imports: [
     CommonModule,
-    MessagesRoutingModule
+    MessagesRoutingModule,
+    MessagesModule,
+    MessageModule
   ]
 })
-export class MessagesModule { }
+export class AppMessagesModule { }
