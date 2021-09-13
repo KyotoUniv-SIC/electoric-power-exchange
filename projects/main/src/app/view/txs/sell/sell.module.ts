f15767a4329e5707fd340a1c7d72ccc15ebbb8ac
Senna Ohmura <29295263+Senna46@users.noreply.github.com>
2021-09-13 15:05:10 +0900

    feat: sell component

--- projects/main/src/app/view/txs/sell/sell.module.ts
+++ projects/main/src/app/view/txs/sell/sell.module.ts
@@ -1,11 +1,12 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { SellComponent } from "./sell.component";
+import { SellComponent } from './sell.component';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [SellComponent],
-  imports: [CommonModule, RouterModule],
-  exports: [SellComponent]
+  imports: [CommonModule, RouterModule, FormsModule, ReactiveFormsModule],
+  exports: [SellComponent],
 })
 export class SellModule {}
