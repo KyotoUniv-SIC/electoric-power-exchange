4ffb537e44089d29e1066e828ba0eb30e85ff7ea
Tomosuke0930 <84496536+Tomosuke0930@users.noreply.github.com>
2021-09-15 18:54:32 +0900

    fix: design-angular-material-buy/sell

--- projects/main/src/app/view/txs/sell/sell.module.ts
+++ projects/main/src/app/view/txs/sell/sell.module.ts
@@ -3,10 +3,13 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [SellComponent],
-  imports: [CommonModule, RouterModule, FormsModule, ReactiveFormsModule],
+  imports: [CommonModule, RouterModule, FormsModule, ReactiveFormsModule, MatButtonModule,MatSelectModule, MatInputModule ],
   exports: [SellComponent],
 })
 export class SellModule {}
