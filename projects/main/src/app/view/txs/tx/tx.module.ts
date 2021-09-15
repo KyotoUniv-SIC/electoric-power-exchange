ce88f1f8632b92ec561c7c90f9fa79fef9c6c9fc
Tomosuke0930 <84496536+Tomosuke0930@users.noreply.github.com>
2021-09-15 16:17:32 +0900

    fix: change-underline&button-color

--- projects/main/src/app/view/txs/tx/tx.module.ts
+++ projects/main/src/app/view/txs/tx/tx.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { TxComponent } from "./tx.component";
 import { RouterModule } from '@angular/router';
+import { MaterialModule } from "../../../material.module";
+import {MatChipsModule} from '@angular/material/chips';
 
 @NgModule({
   declarations: [TxComponent],
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule,MaterialModule,MatChipsModule],
   exports: [TxComponent]
 })
 export class TxModule {}
