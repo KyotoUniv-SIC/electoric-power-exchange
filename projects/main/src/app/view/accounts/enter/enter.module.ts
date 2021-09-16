4ff59ab4761f6cadb28ca26b99183ddffc7cc9c3
Tomosuke0930 <84496536+Tomosuke0930@users.noreply.github.com>
2021-09-16 14:13:58 +0900

    fix: enter_change-angular-material

--- projects/main/src/app/view/accounts/enter/enter.module.ts
+++ projects/main/src/app/view/accounts/enter/enter.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { EnterComponent } from "./enter.component";
 import { RouterModule } from '@angular/router';
+import { MaterialModule } from "../../../material.module";
+import { MatInputModule } from "@angular/material/input";
 
 @NgModule({
   declarations: [EnterComponent],
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule,MaterialModule,MatInputModule],
   exports: [EnterComponent]
 })
 export class EnterModule {}
