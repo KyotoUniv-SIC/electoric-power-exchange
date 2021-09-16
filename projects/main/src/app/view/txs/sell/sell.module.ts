893034f956289b8c6369732afcca089288c29a8d
Senna Ohmura <29295263+Senna46@users.noreply.github.com>
2021-09-16 10:26:24 +0900

    fix: buy sell

--- projects/main/src/app/view/txs/sell/sell.module.ts
+++ projects/main/src/app/view/txs/sell/sell.module.ts
@@ -1,15 +1,13 @@
+import { MaterialModule } from '../../../material.module';
 import { SellComponent } from './sell.component';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSelectModule } from '@angular/material/select';
-import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [SellComponent],
-  imports: [CommonModule, RouterModule, FormsModule, ReactiveFormsModule, MatButtonModule,MatSelectModule, MatInputModule ],
+  imports: [CommonModule, RouterModule, FormsModule, ReactiveFormsModule, MaterialModule],
   exports: [SellComponent],
 })
 export class SellModule {}
--- projects/main/src/app/view/txs/tx/tx.module.ts
+++ projects/main/src/app/view/txs/tx/tx.module.ts
@@ -1,13 +1,13 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { TxComponent } from "./tx.component";
+import { MaterialModule } from '../../../material.module';
+import { TxComponent } from './tx.component';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { MatChipsModule } from '@angular/material/chips';
 import { RouterModule } from '@angular/router';
-import { MaterialModule } from "../../../material.module";
-import {MatChipsModule} from '@angular/material/chips';
 
 @NgModule({
   declarations: [TxComponent],
-  imports: [CommonModule, RouterModule,MaterialModule,MatChipsModule],
-  exports: [TxComponent]
+  imports: [CommonModule, RouterModule, MaterialModule, MatChipsModule],
+  exports: [TxComponent],
 })
 export class TxModule {}
