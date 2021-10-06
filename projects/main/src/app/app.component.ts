18defb0add05f7c8019d289ced838f18a1b0cdd1
Senna Ohmura <29295263+Senna46@users.noreply.github.com>
2021-10-06 18:43:33 +0900

    chore: hide nav in home

--- projects/main/src/app/app.component.ts
+++ projects/main/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'main';
+  hiddenPath: string[] = ['accounts', 'txs', 'messages'];
+  hidden!: boolean;
+  constructor(public router: Router) {}
+  ngOnInit() {
+    this.router.events.pipe(filter((f) => f instanceof NavigationEnd)).subscribe((s: any) => {
+      this.hidden = this.hiddenPath.some((e) => s.url.includes(e));
+    });
+  }
 }
