55c2dc7720141ab44d8a999bd2a42fec04b6a85b
Senna Ohmura <29295263+Senna46@users.noreply.github.com>
2021-10-07 18:35:50 +0900

    chore: dashboard

---
+++ projects/main/src/app/page/dashboard/dashboard.module.ts
@@ -0,0 +1,9 @@
+import { DashboardComponent } from './dashboard.component';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+  declarations: [DashboardComponent],
+  imports: [CommonModule],
+})
+export class AppDashboardModule {}
