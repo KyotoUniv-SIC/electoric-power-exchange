1b9798adedce129a554858d797216e9edb60d417
Senna Ohmura <29295263+Senna46@users.noreply.github.com>
2021-10-11 19:14:40 +0900

    chore: del file

--- data/get-XRP-txs.mjs
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env zx
-const response = axios.get('https://data.ripple.com/v2/payments/');
-
-let csvContent = 'data:text/csv;charset=utf-8,' + response.data.map((e) => e.join(',')).join('\n');
