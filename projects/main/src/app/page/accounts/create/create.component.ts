7c9fa9ae27cce5b73e24854686e0a10fa265a4fa
Senna Ohmura <29295263+Senna46@users.noreply.github.com>
2021-11-08 17:20:29 +0900

    fix: material version mismatch

--- projects/main/src/app/page/accounts/create/create.component.ts
+++ projects/main/src/app/page/accounts/create/create.component.ts
@@ -13,6 +13,7 @@ export class CreateComponent implements OnInit {
   ngOnInit(): void {}
 
   async onSubmit($event: CreateOnSubmitEvent) {
+    console.log($event.mail, $event.password, $event.name);
     this.auth.signup(
       {
         type: 'email',
