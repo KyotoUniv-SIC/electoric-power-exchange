33547164be1501d9a8d6fb7003f02fe753bd557d
Senna Ohmura <29295263+Senna46@users.noreply.github.com>
2021-11-09 14:25:20 +0900

    feat: tx autoid

--- projects/shared/src/lib/services/ask-requests/ask-request.infrastructure.service.ts
+++ projects/shared/src/lib/services/ask-requests/ask-request.infrastructure.service.ts
@@ -1,40 +1,51 @@
-import { Injectable } from '@angular/core';
-import { Firestore, collection, collectionGroup, query, QueryConstraint, doc, getDoc, docData, getDocs, collectionData, setDoc, serverTimestamp } from '@angular/fire/firestore';
+import { askAutoId } from './ask-auto-id';
 import { IAskRequestInfrastructureService } from './ask-request.service';
+import { Injectable } from '@angular/core';
+import {
+  Firestore,
+  collection,
+  collectionGroup,
+  query,
+  QueryConstraint,
+  doc,
+  getDoc,
+  docData,
+  getDocs,
+  collectionData,
+  setDoc,
+  serverTimestamp,
+} from '@angular/fire/firestore';
 import { AskRequest } from '@local/common';
 import { AskRequestFirestore } from '@local/common';
 
 @Injectable({
   providedIn: 'root',
 })
-export class AskRequestInfrastructureService
-  implements IAskRequestInfrastructureService {
-
+export class AskRequestInfrastructureService implements IAskRequestInfrastructureService {
   constructor(private readonly firestore: Firestore) {}
 
   collection(...queryConstraints: QueryConstraint[]) {
     const ref = collection(this.firestore, AskRequestFirestore.collectionPath());
 
-    return (queryConstraints.length > 0
-      ? query(ref, ...queryConstraints) : ref).withConverter(AskRequestFirestore.converter);
+    return (queryConstraints.length > 0 ? query(ref, ...queryConstraints) : ref).withConverter(AskRequestFirestore.converter);
   }
 
   collectionGroup(...queryConstraints: QueryConstraint[]) {
     const ref = collectionGroup(this.firestore, AskRequestFirestore.collectionID);
 
-    return (queryConstraints.length > 0
-      ? query(ref, ...queryConstraints) : ref).withConverter(AskRequestFirestore.converter);
+    return (queryConstraints.length > 0 ? query(ref, ...queryConstraints) : ref).withConverter(AskRequestFirestore.converter);
   }
 
   document(id?: string) {
     const ref = collection(this.firestore, AskRequestFirestore.collectionPath());
 
-    return (id ? doc(this.firestore, ref.path, id) : doc(this.firestore, ref.path)).withConverter(AskRequestFirestore.converter);
+    return (id ? doc(this.firestore, ref.path, id) : doc(this.firestore, ref.path, askAutoId())).withConverter(
+      AskRequestFirestore.converter,
+    );
   }
 
   get(id: string) {
-    return getDoc(this.document(id))
-      .then(snapshot => snapshot.data());
+    return getDoc(this.document(id)).then((snapshot) => snapshot.data());
   }
 
   get$(id: string) {
@@ -42,8 +53,7 @@ export class AskRequestInfrastructureService
   }
 
   list() {
-    return getDocs(this.collection())
-      .then(snapshots => snapshots.docs.map(doc => doc.data()))
+    return getDocs(this.collection()).then((snapshots) => snapshots.docs.map((doc) => doc.data()));
   }
 
   list$() {
@@ -51,8 +61,7 @@ export class AskRequestInfrastructureService
   }
 
   listGroup() {
-    return getDocs(this.collectionGroup())
-      .then(snapshots => snapshots.docs.map(doc => doc.data()))
+    return getDocs(this.collectionGroup()).then((snapshots) => snapshots.docs.map((doc) => doc.data()));
   }
 
   listGroup$() {
