b7eb18375bc97819bcb486704ee24796304e7da4
Senna46 <29295263+Senna46@users.noreply.github.com>
2021-11-10 16:56:42 +0900

    fix: account info nullpo

--- projects/main/src/app/page/accounts/account/account.component.ts
+++ projects/main/src/app/page/accounts/account/account.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { getAuth, User } from '@angular/fire/auth';
 import { ActivatedRoute } from '@angular/router';
-import { User } from '@local/common';
-import { AuthService } from 'projects/shared/src/lib/services/auth/auth.service';
-import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-account',
@@ -11,10 +8,13 @@ import { Observable, of } from 'rxjs';
   styleUrls: ['./account.component.css'],
 })
 export class AccountComponent implements OnInit {
-  user$: Observable<User | undefined>;
+  user: User | undefined;
 
-  constructor(private route: ActivatedRoute, private db: AngularFirestore, private auth: AuthService) {
-    this.user$ = auth.currentUser$;
+  constructor(private route: ActivatedRoute) {
+    const auth = getAuth();
+    if (auth.currentUser !== null) {
+      this.user = auth.currentUser;
+    }
   }
   ngOnInit(): void {}
 }
