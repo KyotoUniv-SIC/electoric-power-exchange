75cdc85865c8d8ac16530a630c0915df9f7524ab
Senna Ohmura <29295263+Senna46@users.noreply.github.com>
2021-11-22 17:54:05 +0900

    fix: messagesを削除、今後はchatsで

--- projects/main/src/app/app-routing.module.ts
+++ projects/main/src/app/app-routing.module.ts
@@ -20,8 +20,8 @@ const routes: Routes = [
     canActivate: [AuthGuard],
   },
   {
-    path: 'messages',
-    loadChildren: () => import('./page/messages/messages.module').then((m) => m.AppMessagesModule),
+    path: 'chats',
+    loadChildren: () => import('./page/chats/chats.module').then((m) => m.AppChatsModule),
     canActivate: [AuthGuard],
   },
   {
