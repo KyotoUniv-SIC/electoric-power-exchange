f1325dda0d5928b6f14bfbaf9fda0823149db5b6
Senna46 <29295263+Senna46@users.noreply.github.com>
2021-11-29 21:33:31 +0900

    feat: function

---
+++ functions/src/primary-bid/index.ts
@@ -0,0 +1,2 @@
+// eslint-disable-next-line camelcase
+export * as primary_bid from './module';
---
+++ functions/src/primary-bid/module.ts
@@ -0,0 +1,56 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+
+/* eslint-disable require-jsdoc */
+import { PrimaryBid, PrimaryBidFirestore } from '@local/common';
+import * as admin from 'firebase-admin';
+
+export * from './controller';
+
+export function collection() {
+  return admin
+    .firestore()
+    .collection(PrimaryBidFirestore.collectionPath())
+    .withConverter(PrimaryBidFirestore.converter as any);
+}
+
+export function collectionGroup() {
+  return admin
+    .firestore()
+    .collectionGroup(PrimaryBidFirestore.collectionID)
+    .withConverter(PrimaryBidFirestore.converter as any);
+}
+
+export function document(id?: string) {
+  const col = collection();
+  return id ? col.doc(id) : col.doc();
+}
+
+export async function get(id: string) {
+  return await document(id)
+    .get()
+    .then((snapshot) => snapshot.data() as PrimaryBid);
+}
+
+export async function create(data: PrimaryBid) {
+  const doc = document();
+  data.id = doc.id;
+
+  const now = admin.firestore.Timestamp.now();
+  data.created_at = now;
+  data.updated_at = now;
+
+  await doc.set(data);
+}
+
+export async function update(data: PrimaryBid) {
+  const now = admin.firestore.Timestamp.now();
+  data.updated_at = now;
+
+  await document(data.id).update(data);
+}
+
+export async function delete_(id: string) {
+  await document(id).delete();
+}
