a7ec2775fcbd8b8a1e651159b35cdf99c4b83430
Senna46 <29295263+Senna46@users.noreply.github.com>
2021-12-03 14:26:54 +0900

    chore: name

---
+++ functions/src/single-price-renewable-settlements/renewable-contract.ts
@@ -0,0 +1,81 @@
+/* eslint-disable camelcase */
+import { single_price_renewable_settlement } from '.';
+import { renewable_ask } from '../renewable-asks';
+import { renewable_bid } from '../renewable-bids';
+import { SinglePriceRenewableSettlement } from '@local/common';
+import * as functions from 'firebase-functions';
+
+exports.scheduledFunctionCrontab = functions.pubsub
+  .schedule('0 0 * * *')
+  .timeZone('Asia/Tokyo') // Users can choose timezone - default is America/Los_Angeles
+  .onRun(async () => {
+    // Bidを価格の高い順に並び替える
+    const renewableBids = await renewable_bid.list();
+    if (!renewableBids || !renewableBids[0]) {
+      return;
+    }
+    const sortRenewableBids = renewableBids.sort((first, second) => second.price - first.price);
+
+    // Askを価格の低い順に並び替える
+    const renewableAsks = await renewable_ask.list();
+    if (!renewableAsks || !renewableAsks[0]) {
+      return;
+    }
+    const sortRenewableAsks = renewableAsks.sort((first, second) => first.price - second.price);
+
+    // Bidの量の推移を配列にする
+    let sumBidAmount = 0;
+    const sumBidAmountHistory = [];
+    for (const bid of sortRenewableBids) {
+      sumBidAmount += bid.amount;
+      sumBidAmountHistory.push(sumBidAmount);
+    }
+
+    // Askの量の推移を配列にする
+    let sumAskAmount = 0;
+    const sumAskAmountHistory = [];
+    for (const ask of sortRenewableAsks) {
+      sumAskAmount += ask.amount;
+      sumAskAmountHistory.push(sumAskAmount);
+    }
+
+    // 階段状の累積受給曲線を歩調を合わせて登ることで均衡価格を発見
+    let i = 0;
+    let j = 0;
+    const condition = true;
+    while (condition) {
+      if (sortRenewableBids[i].price <= sortRenewableAsks[j].price) {
+        break;
+      }
+      if (sumBidAmountHistory[i] <= sumAskAmountHistory[j]) {
+        i++;
+      } else {
+        j++;
+      }
+    }
+
+    // i,j両方が0のとき、成約は0になる
+    if (i == 0 && j == 0) {
+      return;
+    } else {
+      // 止まったときの高い方の価格が均衡価格となる
+      const equilibriumPrice =
+        sortRenewableBids[i].price <= sortRenewableAsks[j].price ? sortRenewableAsks[i].price : sortRenewableBids[j].price;
+      // 止まったときの低い方が成約取引量となる
+      const equilibriumAmount =
+        sortRenewableBids[i].amount <= sortRenewableAsks[j].amount ? sortRenewableBids[i].amount : sortRenewableAsks[j].amount;
+      // 一日前の日付を取得
+      const time = new Date();
+      time.setDate(time.getDate() - 1);
+
+      await single_price_renewable_settlement.create(
+        new SinglePriceRenewableSettlement({
+          price: equilibriumPrice,
+          amount: equilibriumAmount,
+          year: time.getFullYear(),
+          month: time.getMonth(),
+          date: time.getDate(),
+        }),
+      );
+    }
+  });
