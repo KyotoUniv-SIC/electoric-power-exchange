ba636f4546e183f99aff0a47923cea219d325b3b
Senna46 <29295263+Senna46@users.noreply.github.com>
2021-12-03 13:33:28 +0900

    fix: search equilibrium

--- functions/src/single-price-normal-settlements/contract.ts
+++ functions/src/single-price-normal-settlements/contract.ts
@@ -17,32 +17,53 @@ exports.scheduledFunctionCrontab = functions.pubsub
     const normalAsks = await normal_ask.list();
     const sortNormalAsks = normalAsks.sort((first, second) => first.price - second.price);
 
+    // Bidの量の推移を配列にする
+    let sumBidAmount = 0;
+    const sumBidAmountHistory = [];
+    for (const bid of sortNormalBids) {
+      sumBidAmount += bid.amount;
+      sumBidAmountHistory.push(sumBidAmount);
+    }
+
+    // Askの量の推移を配列にする
+    let sumAskAmount = 0;
+    const sumAskAmountHistory = [];
+    for (const ask of sortNormalAsks) {
+      sumAskAmount += ask.amount;
+      sumAskAmountHistory.push(sumAskAmount);
+    }
+
+    // 階段状の累積受給曲線を歩調を合わせて登ることで均衡価格を発見
     let i = 0;
     let j = 0;
-    let match = [];
-
-    // 価格の均衡点を探して、matchに記録
-    while (i < sortNormalBids.length && j < sortNormalAsks.length) {
+    const condition = true;
+    while (condition) {
       if (sortNormalBids[i].price <= sortNormalAsks[j].price) {
-        j = j + 1;
+        break;
+      }
+      if (sumBidAmountHistory[i] <= sumAskAmountHistory[j]) {
+        i++;
       } else {
-        i = i + 1;
-        match = [i, j];
+        j++;
       }
     }
 
-    if (!match.length) {
-      const price = sortNormalBids[match[0]].price;
-      const acceptBidsAmount = sortNormalBids.slice(match[0]).reduce((sum, next) => sum + next.amount, 0);
-      const acceptAsksAmount = sortNormalAsks.slice(match[1]).reduce((sum, next) => sum + next.amount, 0);
-      const amount = acceptBidsAmount < acceptAsksAmount ? acceptBidsAmount : acceptAsksAmount;
+    // i,j両方が0のとき、成約は0になる
+    if (i == 0 && j == 0) {
+      return;
+    } else {
+      // 止まったときの高い方の価格が均衡価格となる
+      const equilibriumPrice = sortNormalBids[i].price <= sortNormalAsks[j].price ? sortNormalAsks[i].price : sortNormalBids[j].price;
+      // 止まったときの低い方が成約取引量となる
+      const equilibriumAmount = sortNormalBids[i].amount <= sortNormalAsks[j].amount ? sortNormalBids[i].amount : sortNormalAsks[j].amount;
+      // 一日前の日付を取得
       const time = new Date();
       time.setDate(time.getDate() - 1);
 
       await single_price_normal_settlement.create(
         new SinglePriceNormalSettlement({
-          price: price,
-          amount: amount,
+          price: equilibriumPrice,
+          amount: equilibriumAmount,
           year: time.getFullYear(),
           month: time.getMonth(),
           date: time.getDate(),
