55d31d89ccb52719836ca0a406dd4c26e624856c
Senna46 <29295263+Senna46@users.noreply.github.com>
2021-12-08 14:01:51 +0900

    feat: snapshot=>token付与の順番指定

--- functions/src/primary-asks/primary.ts
+++ functions/src/primary-asks/primary.ts
@@ -1,27 +1,24 @@
 /* eslint-disable camelcase */
 import { primary_ask } from '.';
+import { balance_snapshot } from '../balance-snapshots';
 import { monthly_usage } from '../monthly-usages';
 import { student_account } from '../student-accounts';
 import { PrimaryAsk } from '@local/common';
-import * as functions from 'firebase-functions';
 
-exports.scheduledFunctionCrontab = functions.pubsub
-  .schedule('0 0 1 * *')
-  .timeZone('Asia/Tokyo') // Users can choose timezone - default is America/Los_Angeles
-  .onRun(async () => {
-    const students = await student_account.list();
+balance_snapshot.onCreateHandler.push(async (snapshot, context) => {
+  const students = await student_account.list();
 
-    for (const student of students) {
-      const studentID = student.id;
-      const now = new Date();
-      const monthlyUsage = await monthly_usage.getLastYear(studentID, now);
-      const usageAmount = !monthlyUsage.length ? 120 : monthlyUsage[0].amount_kwh;
-      await primary_ask.create(
-        new PrimaryAsk({
-          account_id: studentID,
-          price: 27,
-          amount: usageAmount,
-        }),
-      );
-    }
-  });
+  for (const student of students) {
+    const studentID = student.id;
+    const now = new Date();
+    const monthlyUsage = await monthly_usage.getLastYear(studentID, now);
+    const usageAmount = !monthlyUsage.length ? 120 : monthlyUsage[0].amount_kwh;
+    await primary_ask.create(
+      new PrimaryAsk({
+        account_id: studentID,
+        price: 27,
+        amount: usageAmount,
+      }),
+    );
+  }
+});
