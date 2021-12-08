1b5fe762cf1c63a3aebc062bd2d11cadff855474
Senna46 <29295263+Senna46@users.noreply.github.com>
2021-12-08 15:21:48 +0900

    feat: settlement作成時にbid, askをhistoryに変えるfunc

--- functions/src/normal-settlements/normal-settlement.ts
+++ functions/src/normal-settlements/normal-settlement.ts
@@ -1,9 +1,11 @@
 /* eslint-disable camelcase */
 import { normal_settlement } from '.';
+import { normal_ask_history } from '../normal-ask-histories';
 import { normal_ask } from '../normal-asks';
+import { normal_bid_history } from '../normal-bid-histories';
 import { normal_bid } from '../normal-bids';
 import { single_price_normal_settlement } from '../single-price-normal-settlements';
-import { NormalSettlement } from '@local/common';
+import { NormalAskHistory, NormalBidHistory, NormalSettlement } from '@local/common';
 
 single_price_normal_settlement.onCreateHandler.push(async (snapshot, context) => {
   const data = snapshot.data()!;
@@ -19,6 +21,31 @@ single_price_normal_settlement.onCreateHandler.push(async (snapshot, context) =>
   const condition = true;
   while (condition) {
     if (sortNormalBids[i] < data.price || sortNormalAsks[j] > data.price) {
+      for (; i < sortNormalBids.length; i++) {
+        await normal_bid_history.create(
+          new NormalBidHistory({
+            account_id: sortNormalBids[i].account_id,
+            price: sortNormalBids[i].price,
+            amount: sortNormalBids[i].amount,
+            is_accepted: false,
+            contract_price: data.price,
+          }),
+        );
+        await normal_bid.delete_(sortNormalBids[i].id);
+      }
+
+      for (; j < sortNormalAsks.length; j++) {
+        await normal_ask_history.create(
+          new NormalAskHistory({
+            account_id: sortNormalAsks[j].account_id,
+            price: sortNormalAsks[j].price,
+            amount: sortNormalAsks[j].amount,
+            is_accepted: false,
+            contract_price: data.price,
+          }),
+        );
+        await normal_ask.delete_(sortNormalAsks[j].id);
+      }
       break;
     }
     if (sortNormalBids[i].amount < sortNormalAsks[j].amount) {
@@ -30,6 +57,29 @@ single_price_normal_settlement.onCreateHandler.push(async (snapshot, context) =>
           amount: sortNormalBids[i].amount,
         }),
       );
+
+      await normal_bid_history.create(
+        new NormalBidHistory({
+          account_id: sortNormalBids[i].account_id,
+          price: sortNormalBids[i].price,
+          amount: sortNormalBids[i].amount,
+          is_accepted: true,
+          contract_price: data.price,
+        }),
+      );
+      await normal_bid.delete_(sortNormalBids[i].id);
+
+      await normal_ask_history.create(
+        new NormalAskHistory({
+          account_id: sortNormalAsks[j].account_id,
+          price: sortNormalAsks[j].price,
+          amount: sortNormalBids[i].amount,
+          is_accepted: true,
+          contract_price: data.price,
+        }),
+      );
+      await normal_ask.delete_(sortNormalAsks[j].id);
+
       sortNormalAsks[j].amount -= sortNormalBids[i].amount;
       i++;
     } else if (sortNormalBids[i].amount > sortNormalAsks[j].amount) {
@@ -41,6 +91,29 @@ single_price_normal_settlement.onCreateHandler.push(async (snapshot, context) =>
           amount: sortNormalAsks[j].amount,
         }),
       );
+
+      await normal_bid_history.create(
+        new NormalBidHistory({
+          account_id: sortNormalBids[i].account_id,
+          price: sortNormalBids[i].price,
+          amount: sortNormalAsks[j].amount,
+          is_accepted: true,
+          contract_price: data.price,
+        }),
+      );
+      await normal_bid.delete_(sortNormalBids[i].id);
+
+      await normal_ask_history.create(
+        new NormalAskHistory({
+          account_id: sortNormalAsks[j].account_id,
+          price: sortNormalAsks[j].price,
+          amount: sortNormalAsks[j].amount,
+          is_accepted: true,
+          contract_price: data.price,
+        }),
+      );
+      await normal_ask.delete_(sortNormalAsks[j].id);
+
       sortNormalBids[i].amount -= sortNormalAsks[j].amount;
       j++;
     } else {
@@ -52,6 +125,30 @@ single_price_normal_settlement.onCreateHandler.push(async (snapshot, context) =>
           amount: sortNormalBids[i].amount,
         }),
       );
+
+      await normal_bid_history.create(
+        new NormalBidHistory({
+          account_id: sortNormalBids[i].account_id,
+          price: sortNormalBids[i].price,
+          amount: sortNormalBids[i].amount,
+          is_accepted: true,
+          contract_price: data.price,
+        }),
+      );
+
+      await normal_bid.delete_(sortNormalBids[i].id);
+
+      await normal_ask_history.create(
+        new NormalAskHistory({
+          account_id: sortNormalAsks[j].account_id,
+          price: sortNormalAsks[j].price,
+          amount: sortNormalBids[i].amount,
+          is_accepted: true,
+          contract_price: data.price,
+        }),
+      );
+      await normal_ask.delete_(sortNormalAsks[j].id);
+
       i++;
       j++;
     }
