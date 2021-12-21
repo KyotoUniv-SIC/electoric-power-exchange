f8cd1a389394fbd60533f0f11f90cd97bd01c569
Senna46 <29295263+Senna46@users.noreply.github.com>
2021-12-21 15:09:39 +0900

    fix: index

--- functions/src/available-balances/balance.ts
+++ functions/src/available-balances/balance.ts
@@ -3,13 +3,13 @@ import { available_balance } from '.';
 import { balance } from '../balances';
 import { AvailableBalance } from '@local/common';
 
-module.exports.balanceUpdate = balance.onUpdateHandler.push(async (snapshot, context) => {
+balance.onUpdateHandler.push(async (snapshot, context) => {
   const data = snapshot.after.data()!;
 
   await available_balance.update(new AvailableBalance(data, data.created_at, data.updated_at));
 });
 
-module.exports.balanceCreate = balance.onCreateHandler.push(async (snapshot, context) => {
+balance.onCreateHandler.push(async (snapshot, context) => {
   const data = snapshot.data()!;
 
   await available_balance.update(new AvailableBalance(data, data.created_at, data.updated_at));
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,7 @@
 /* eslint-disable camelcase */
-// import { account } from './accounts';
-// import { balance } from './balances';
-// import { normal_ask } from './normal-asks';
-// import { normal_bid } from './normal-bids';
-// import { primary_ask } from './primary-asks';
-// import { primary_bid } from './primary-bids';
 import * as admin from 'firebase-admin';
+
+// import * as functions from 'firebase-functions';
 
 admin.initializeApp();
 // Take the text parameter passed to this HTTP endpoint and insert it into
@@ -14,16 +10,33 @@ admin.initializeApp();
 // export const auth_get_users = auth.getUsers;
 // export const auth_remove_user = auth.removeUser;
 // export const account_on_create = account.onCreate;
-// export const primary_ask_on_create = primary_ask.onCreate;
-// export const primary_bid_on_create = primary_bid.onCreate;
-// export const normal_ask_on_create = normal_ask.onCreate;
-// export const normal_bid_on_create = normal_bid.onCreate;
-// export const balance_on_create = balance.onCreate;
-// export const balance_on_update = balance.onUpdate;
 
 const files = {
-  availableBalanceUpdate: './available-balances/balance',
-  balanceSnapShot: './balance-snapshots/primary',
+  accounts: './accounts',
+  admin_accounts: './admin-accounts',
+  available_balances: './available-balances',
+  balance_snapshots: './balance-snapshots',
+  balances: './balances',
+  daily_usages: './daily-usages',
+  discount_prices: './discount-prices',
+  market_statuses: './market-statuses',
+  monthly_payments: './monthly-payments',
+  monthly_usages: './monthly-usages',
+  normal_asks: './normal-asks',
+  normal_ask_histories: './normal-ask-histories',
+  normal_bids: './normal-bids',
+  normal_bid_histories: './normal-bid-histories',
+  normal_settlements: './normal-settlements',
+  primary_asks: './primary-asks',
+  primary_bids: './primary-bids',
+  renewable_asks: './renewable-asks',
+  renewable_ask_histories: './renewable-ask-histories',
+  renewable_bids: './renewable-bids',
+  renewable_bid_histories: './renewable-bid-histories',
+  renewable_settlements: './renewable-settlements',
+  normals: './single-price-normal-settlements',
+  renewables: './single-price-renewable-settlements',
+  student_accounts: './student-accounts',
 };
 
 const loadFunctions = (filesObj: any) => {
