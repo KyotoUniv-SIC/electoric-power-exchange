208fe725c51ac4df084282fe5163ab3e35445f44
Senna46 <29295263+Senna46@users.noreply.github.com>
2021-12-24 15:32:25 +0900

    fix: firestore import

--- functions/src/normal-asks/controller.ts
+++ functions/src/normal-asks/controller.ts
@@ -1,13 +1,13 @@
 import { FirestoreCreateHandler, FirestoreDeleteHandler, FirestoreUpdateHandler } from '../triggers';
 import { isTriggeredOnce } from '../triggers/module';
-import { AccountFirestore } from '@local/common';
+import { NormalAskFirestore } from '@local/common';
 import * as functions from 'firebase-functions';
 
 export const onCreateHandler: FirestoreCreateHandler[] = [];
 export const onUpdateHandler: FirestoreUpdateHandler[] = [];
 export const onDeleteHandler: FirestoreDeleteHandler[] = [];
 
-module.exports.onCreate = functions.firestore.document(AccountFirestore.virtualPath).onCreate(async (snapshot, context) => {
+module.exports.onCreate = functions.firestore.document(NormalAskFirestore.virtualPath).onCreate(async (snapshot, context) => {
   if (await isTriggeredOnce(context.eventId)) {
     return;
   }
@@ -21,7 +21,7 @@ module.exports.onCreate = functions.firestore.document(AccountFirestore.virtualP
   }
 });
 
-module.exports.onUpdate = functions.firestore.document(AccountFirestore.virtualPath).onUpdate(async (snapshot, context) => {
+module.exports.onUpdate = functions.firestore.document(NormalAskFirestore.virtualPath).onUpdate(async (snapshot, context) => {
   if (await isTriggeredOnce(context.eventId)) {
     return;
   }
@@ -35,7 +35,7 @@ module.exports.onUpdate = functions.firestore.document(AccountFirestore.virtualP
   }
 });
 
-module.exports.onDelete = functions.firestore.document(AccountFirestore.virtualPath).onDelete(async (snapshot, context) => {
+module.exports.onDelete = functions.firestore.document(NormalAskFirestore.virtualPath).onDelete(async (snapshot, context) => {
   if (await isTriggeredOnce(context.eventId)) {
     return;
   }
--- functions/src/single-price-normal-settlements/controller.ts
+++ functions/src/single-price-normal-settlements/controller.ts
@@ -1,50 +1,56 @@
 import { FirestoreCreateHandler, FirestoreDeleteHandler, FirestoreUpdateHandler } from '../triggers';
 import { isTriggeredOnce } from '../triggers/module';
-import { AccountFirestore } from '@local/common';
+import { SinglePriceNormalSettlementFirestore } from '@local/common';
 import * as functions from 'firebase-functions';
 
 export const onCreateHandler: FirestoreCreateHandler[] = [];
 export const onUpdateHandler: FirestoreUpdateHandler[] = [];
 export const onDeleteHandler: FirestoreDeleteHandler[] = [];
 
-module.exports.onCreate = functions.firestore.document(AccountFirestore.virtualPath).onCreate(async (snapshot, context) => {
-  if (await isTriggeredOnce(context.eventId)) {
-    return;
-  }
-
-  for (const handler of onCreateHandler) {
-    try {
-      await handler(snapshot, context);
-    } catch (e) {
-      console.error(e);
+module.exports.onCreate = functions.firestore
+  .document(SinglePriceNormalSettlementFirestore.virtualPath)
+  .onCreate(async (snapshot, context) => {
+    if (await isTriggeredOnce(context.eventId)) {
+      return;
     }
-  }
-});
 
-module.exports.onUpdate = functions.firestore.document(AccountFirestore.virtualPath).onUpdate(async (snapshot, context) => {
-  if (await isTriggeredOnce(context.eventId)) {
-    return;
-  }
-
-  for (const handler of onUpdateHandler) {
-    try {
-      await handler(snapshot, context);
-    } catch (e) {
-      console.error(e);
+    for (const handler of onCreateHandler) {
+      try {
+        await handler(snapshot, context);
+      } catch (e) {
+        console.error(e);
+      }
     }
-  }
-});
+  });
 
-module.exports.onDelete = functions.firestore.document(AccountFirestore.virtualPath).onDelete(async (snapshot, context) => {
-  if (await isTriggeredOnce(context.eventId)) {
-    return;
-  }
-
-  for (const handler of onDeleteHandler) {
-    try {
-      await handler(snapshot, context);
-    } catch (e) {
-      console.error(e);
+module.exports.onUpdate = functions.firestore
+  .document(SinglePriceNormalSettlementFirestore.virtualPath)
+  .onUpdate(async (snapshot, context) => {
+    if (await isTriggeredOnce(context.eventId)) {
+      return;
     }
-  }
-});
+
+    for (const handler of onUpdateHandler) {
+      try {
+        await handler(snapshot, context);
+      } catch (e) {
+        console.error(e);
+      }
+    }
+  });
+
+module.exports.onDelete = functions.firestore
+  .document(SinglePriceNormalSettlementFirestore.virtualPath)
+  .onDelete(async (snapshot, context) => {
+    if (await isTriggeredOnce(context.eventId)) {
+      return;
+    }
+
+    for (const handler of onDeleteHandler) {
+      try {
+        await handler(snapshot, context);
+      } catch (e) {
+        console.error(e);
+      }
+    }
+  });
