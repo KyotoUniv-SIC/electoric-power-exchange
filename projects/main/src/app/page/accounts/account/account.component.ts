22d83e23e7fe771b44164290b36d504a28faf0da
KokiKumagai <94220982+KokiKumagai@users.noreply.github.com>
2021-12-27 19:59:13 +0900

    feat: account-detail

--- projects/main/src/app/page/accounts/account/account.component.ts
+++ projects/main/src/app/page/accounts/account/account.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { getAuth, User } from '@angular/fire/auth';
 import { ActivatedRoute } from '@angular/router';
+import { Balance, MonthlyPayment } from '@local/common';
+import { BalanceApplicationService } from 'projects/shared/src/lib/services/student-accounts/balances/balance.application.service';
+import { MonthlyPaymentApplicationService } from 'projects/shared/src/lib/services/student-accounts/monthly-payments/monthly-payment.application.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-account',
@@ -9,12 +13,24 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class AccountComponent implements OnInit {
   user: User | undefined;
+  balances$: Observable<Balance> | undefined;
+  monthlyPayments$: Observable<MonthlyPayment[]> | undefined;
 
-  constructor(private route: ActivatedRoute) {
+  constructor(
+    private route: ActivatedRoute,
+    private readonly balanceApp: BalanceApplicationService,
+    private readonly MonthlyPaymentApp: MonthlyPaymentApplicationService,
+  ) {
     const auth = getAuth();
     if (auth.currentUser !== null) {
       this.user = auth.currentUser;
     }
+    const accountID = auth.currentUser?.uid;
+    if (!accountID) {
+      return;
+    }
+    this.monthlyPayments$ = this.MonthlyPaymentApp.list$(accountID);
+    this.balances$ = this.balanceApp.getByUid$(accountID);
   }
   ngOnInit(): void {}
 }
--- projects/main/src/app/view/accounts/account/account.component.ts
+++ projects/main/src/app/view/accounts/account/account.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { User } from '@firebase/auth';
+import { Balance, MonthlyPayment } from 'common/dist/cjs';
 
 @Component({
   selector: 'view-account',
@@ -9,7 +10,10 @@ import { User } from '@firebase/auth';
 export class AccountComponent implements OnInit {
   @Input()
   user?: User | null;
-
+  @Input()
+  monthlyPayments?: MonthlyPayment[] | null;
+  @Input()
+  balances?: Balance | null;
   constructor() {}
 
   ngOnInit(): void {}
