0056126778da5eb50884506989c41a37c8b1125e
Senna46 <29295263+Senna46@users.noreply.github.com>
2022-01-17 16:00:29 +0900

    fix: update include id

--- functions/src/single-price-normal-settlements/normal-contract.ts
+++ functions/src/single-price-normal-settlements/normal-contract.ts
@@ -60,10 +60,11 @@ module.exports.normalContract = functions.pubsub
     // i,j両方が0のとき、成約は0になる
     if (i == 0 && j == 0) {
       const marketStatus = await market_status.getToday();
+      console.log('UPX成約は0です。', marketStatus);
       if (!marketStatus.length) {
         await market_status.create(new MarketStatus({ is_finished_normal: true, is_finished_renewable: false }));
       } else {
-        await market_status.update(new MarketStatus({ is_finished_normal: true }));
+        await market_status.update(new MarketStatus({ id: marketStatus[0].id, is_finished_normal: true }));
       }
 
       for (const bid of sortNormalBids) {
