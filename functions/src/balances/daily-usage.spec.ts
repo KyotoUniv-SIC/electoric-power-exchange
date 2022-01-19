993415dbb941f7d40e9330ac5c76247a4b8f0822
Senna46 <29295263+Senna46@users.noreply.github.com>
2022-01-19 15:44:42 +0900

    feat: daily usage test

---
+++ functions/src/balances/daily-usage.spec.ts
@@ -0,0 +1,32 @@
+import { Balance, DailyUsage } from '@local/common';
+
+describe('Daily Usage Test', () => {
+  it('Daily withdrawal', () => {
+    const data = new DailyUsage({ student_account_id: 'test01', amount_kwh: 5 });
+    // const accountBalance = [new Balance({ student_account_id: 'test01', amount_upx: 100, amount_spx: 100 })];
+    const accountBalance = [new Balance({ student_account_id: 'test01', amount_upx: 100, amount_spx: 0 })];
+    if (!accountBalance.length) {
+      return;
+    }
+    if (data.amount_kwh < accountBalance[0].amount_spx) {
+      const updatedBalance = new Balance({
+        id: accountBalance[0].id,
+        student_account_id: data.student_account_id,
+        amount_upx: accountBalance[0].amount_upx,
+        amount_spx: accountBalance[0].amount_spx - data.amount_kwh,
+      });
+      console.log(updatedBalance);
+      expect(updatedBalance.amount_spx).toBe(95);
+    } else {
+      const spxShortage = data.amount_kwh - accountBalance[0].amount_spx;
+      const updatedBalance = new Balance({
+        id: accountBalance[0].id,
+        student_account_id: data.student_account_id,
+        amount_upx: accountBalance[0].amount_upx - spxShortage,
+        amount_spx: 0,
+      });
+      console.log(updatedBalance);
+      expect(updatedBalance.amount_upx).toBe(95);
+    }
+  });
+});
