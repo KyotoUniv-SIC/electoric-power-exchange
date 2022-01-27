e6bb879d348e47a45416c84ea95824d7a837e7dc
Senna46 <29295263+Senna46@users.noreply.github.com>
2022-01-27 17:37:58 +0900

    feat: studentaccount作成時にxrpl開設

--- functions/src/student-accounts/xrpl.ts
+++ functions/src/student-accounts/xrpl.ts
@@ -5,18 +5,64 @@
 /* eslint-disable camelcase */
 import { student_account } from '.';
 import { account_private } from '../account-privates';
+import { admin_account } from '../admin-accounts';
 import { AccountPrivate } from '@local/common';
 
 student_account.onCreateHandler.push(async (snapshot, context) => {
   const data = snapshot.data()!;
+  const adminAccount = await admin_account.getByName('admin');
   const xrpl = require('xrpl');
   async function createWallet() {
     const TEST_NET = 'wss://s.altnet.rippletest.net:51233';
     const client = new xrpl.Client(TEST_NET);
     await client.connect();
 
-    const testWallet = xrpl.Wallet.generate();
-    console.log(testWallet);
+    const fundResult = await client.fundWallet();
+    const testWallet = fundResult.wallet;
+    const normalDenom = 'UPX';
+    const renewableDenom = 'SPX';
+    const issuerAddress = adminAccount[0].xrp_address_cold;
+    const trustSetTxNormal = {
+      TransactionType: 'TrustSet',
+      Account: testWallet.address,
+      LimitAmount: {
+        currency: normalDenom,
+        issuer: issuerAddress,
+        // Large limit, arbitrarily chosen
+        value: '10000000000',
+      },
+    };
+
+    const tsPreparedNormal = await client.autofill(trustSetTxNormal);
+    const tsSignedNormal = testWallet.sign(tsPreparedNormal);
+    const tsResultNormal = await client.submitAndWait(tsSignedNormal.tx_blob);
+    if (tsResultNormal.result.meta.TransactionResult == 'tesSUCCESS') {
+      console.log(`Transaction succeeded: https://testnet.xrpl.org/transactions/${tsSignedNormal.hash}`);
+    } else {
+      // eslint-disable-next-line no-throw-literal
+      throw `Error sending transaction: ${tsResultNormal.result.meta.TransactionResult}`;
+    }
+
+    const trustSetTxRenewable = {
+      TransactionType: 'TrustSet',
+      Account: testWallet.address,
+      LimitAmount: {
+        currency: renewableDenom,
+        issuer: issuerAddress,
+        // Large limit, arbitrarily chosen
+        value: '10000000000',
+      },
+    };
+
+    const tsPreparedRenewable = await client.autofill(trustSetTxRenewable);
+    const tsSignedRenewable = testWallet.sign(tsPreparedRenewable);
+    const tsResultRenewable = await client.submitAndWait(tsSignedRenewable.tx_blob);
+    if (tsResultRenewable.result.meta.TransactionResult == 'tesSUCCESS') {
+      console.log(`Transaction succeeded: https://testnet.xrpl.org/transactions/${tsSignedRenewable.hash}`);
+    } else {
+      // eslint-disable-next-line no-throw-literal
+      throw `Error sending transaction: ${tsResultRenewable.result.meta.TransactionResult}`;
+    }
     client.disconnect();
     return testWallet;
   }
