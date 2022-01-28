8811bd2fd6bd1344a862fd9eb949999436dc8656
Senna46 <29295263+Senna46@users.noreply.github.com>
2022-01-28 14:25:47 +0900

    feat: 試験的にアカウント作成時に50ずつトークンを付与する

--- functions/src/student-accounts/xrpl.ts
+++ functions/src/student-accounts/xrpl.ts
@@ -6,11 +6,13 @@
 import { student_account } from '.';
 import { account_private } from '../account-privates';
 import { admin_account } from '../admin-accounts';
+import { admin_private } from '../admin-privates';
 import { AccountPrivate } from '@local/common';
 
 student_account.onCreateHandler.push(async (snapshot, context) => {
   const data = snapshot.data()!;
   const adminAccount = await admin_account.getByName('admin');
+  const adminPrivate = await admin_private.list(adminAccount[1].id);
   const xrpl = require('xrpl');
   async function createWallet() {
     const TEST_NET = 'wss://s.altnet.rippletest.net:51233';
@@ -18,13 +20,13 @@ student_account.onCreateHandler.push(async (snapshot, context) => {
     await client.connect();
 
     const fundResult = await client.fundWallet();
-    const testWallet = fundResult.wallet;
+    const wallet = fundResult.wallet;
     const normalDenom = 'UPX';
     const renewableDenom = 'SPX';
     const issuerAddress = adminAccount[0].xrp_address_cold;
     const trustSetTxNormal = {
       TransactionType: 'TrustSet',
-      Account: testWallet.address,
+      Account: wallet.address,
       LimitAmount: {
         currency: normalDenom,
         issuer: issuerAddress,
@@ -34,7 +36,7 @@ student_account.onCreateHandler.push(async (snapshot, context) => {
     };
 
     const tsPreparedNormal = await client.autofill(trustSetTxNormal);
-    const tsSignedNormal = testWallet.sign(tsPreparedNormal);
+    const tsSignedNormal = wallet.sign(tsPreparedNormal);
     const tsResultNormal = await client.submitAndWait(tsSignedNormal.tx_blob);
     if (tsResultNormal.result.meta.TransactionResult == 'tesSUCCESS') {
       console.log(`Transaction succeeded: https://testnet.xrpl.org/transactions/${tsSignedNormal.hash}`);
@@ -45,7 +47,7 @@ student_account.onCreateHandler.push(async (snapshot, context) => {
 
     const trustSetTxRenewable = {
       TransactionType: 'TrustSet',
-      Account: testWallet.address,
+      Account: wallet.address,
       LimitAmount: {
         currency: renewableDenom,
         issuer: issuerAddress,
@@ -55,7 +57,7 @@ student_account.onCreateHandler.push(async (snapshot, context) => {
     };
 
     const tsPreparedRenewable = await client.autofill(trustSetTxRenewable);
-    const tsSignedRenewable = testWallet.sign(tsPreparedRenewable);
+    const tsSignedRenewable = wallet.sign(tsPreparedRenewable);
     const tsResultRenewable = await client.submitAndWait(tsSignedRenewable.tx_blob);
     if (tsResultRenewable.result.meta.TransactionResult == 'tesSUCCESS') {
       console.log(`Transaction succeeded: https://testnet.xrpl.org/transactions/${tsSignedRenewable.hash}`);
@@ -63,8 +65,49 @@ student_account.onCreateHandler.push(async (snapshot, context) => {
       // eslint-disable-next-line no-throw-literal
       throw `Error sending transaction: ${tsResultRenewable.result.meta.TransactionResult}`;
     }
+
+    // アカウント作成時、試験的に50トークンずつ付与する仕様に（精算には含めていない）
+    const sender = xrpl.Wallet.fromSeed(adminPrivate[0].xrp_seed_hot);
+    const sendSPXTx = {
+      TransactionType: 'Payment',
+      Account: sender.address,
+      Amount: {
+        currency: 'SPX',
+        value: String(50),
+        issuer: adminAccount[0].xrp_address_cold,
+      },
+      Destination: wallet.address,
+    };
+    const payPreparedSPX = await client.autofill(sendSPXTx);
+    const paySignedSPX = sender.sign(payPreparedSPX);
+    const payResultSPX = await client.submitAndWait(paySignedSPX.tx_blob);
+    if (payResultSPX.result.meta.TransactionResult == 'tesSUCCESS') {
+      console.log(`Transaction succeeded: https://testnet.xrpl.org/transactions/${paySignedSPX.hash}`);
+    } else {
+      // eslint-disable-next-line no-throw-literal
+      throw `Error sending transaction: ${payResultSPX.result.meta.TransactionResult}`;
+    }
+    const sendUPXTx = {
+      TransactionType: 'Payment',
+      Account: sender.address,
+      Amount: {
+        currency: 'UPX',
+        value: String(50),
+        issuer: adminAccount[0].xrp_address_cold,
+      },
+      Destination: wallet.address,
+    };
+    const payPreparedUPX = await client.autofill(sendUPXTx);
+    const paySignedUPX = sender.sign(payPreparedUPX);
+    const payResultUPX = await client.submitAndWait(paySignedUPX.tx_blob);
+    if (payResultUPX.result.meta.TransactionResult == 'tesSUCCESS') {
+      console.log(`Transaction succeeded: https://testnet.xrpl.org/transactions/${paySignedUPX.hash}`);
+    } else {
+      // eslint-disable-next-line no-throw-literal
+      throw `Error sending transaction: ${payResultUPX.result.meta.TransactionResult}`;
+    }
     client.disconnect();
-    return testWallet;
+    return wallet;
   }
   const wallet = await createWallet();
 
