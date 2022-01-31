19e7327650c3443d7bd1ad8cc387f8f58de6f7d8
Senna46 <29295263+Senna46@users.noreply.github.com>
2022-01-31 15:56:54 +0900

    feat: 月末の全トークンの償還を追加、残高不足を反映

--- functions/src/monthly-payments/balance-snapshot.ts
+++ functions/src/monthly-payments/balance-snapshot.ts
@@ -1,8 +1,13 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+
 /* eslint-disable camelcase */
 import { monthly_payment } from '.';
+import { account_private } from '../account-privates';
+import { admin_account } from '../admin-accounts';
 import { balance_snapshot } from '../balance-snapshots';
 import { daily_usage } from '../daily-usages';
 import { discount_price } from '../discount-prices';
+import { insufficient_balance } from '../insufficient-balances';
 import { monthly_usage } from '../monthly-usages';
 import { normal_ask_history } from '../normal-ask-histories';
 import { normal_bid_history } from '../normal-bid-histories';
@@ -15,7 +20,11 @@ import { MonthlyPayment, MonthlyUsage } from '@local/common';
 
 balance_snapshot.onCreateHandler.push(async (snapshot, context) => {
   const data = snapshot.data()!;
-  const tokens = data.amount_upx + data.amount_spx;
+  const insufficiencies = (await insufficient_balance.listLastMonth(data.student_account_id)).reduce(
+    (sum, element) => sum + element.amount,
+    0,
+  );
+  const tokens = data.amount_upx + data.amount_spx - insufficiencies;
 
   const primaryBids = await primary_bid.getLatest(data.student_account_id);
   const normalBids = await normal_bid_history.listLastMonth(data.student_account_id);
@@ -76,4 +85,61 @@ balance_snapshot.onCreateHandler.push(async (snapshot, context) => {
   await monthly_usage.create(
     new MonthlyUsage({ student_account_id: data.student_account_id, year: date.getFullYear(), month: date.getMonth(), amount_kwh: usage }),
   );
+
+  const accountPrivate = await account_private.list(data.student_account_id);
+  if (!accountPrivate.length) {
+    console.log(data.student_account_id, 'no XRP address');
+    return;
+  }
+  const xrpl = require('xrpl');
+  const TEST_NET = 'wss://s.altnet.rippletest.net:51233';
+  const client = new xrpl.Client(TEST_NET);
+  const adminAccount = await admin_account.getByName('admin');
+  await client.connect();
+  const sender = xrpl.Wallet.fromSeed(accountPrivate[0].xrp_seed);
+  const amountSPX = data.amount_spx - insufficiencies;
+  const amountUPX = amountSPX > 0 ? data.amount_upx : data.amount_upx + amountSPX;
+  if (amountSPX > 0) {
+    const sendTokenTx = {
+      TransactionType: 'Payment',
+      Account: sender.address,
+      Amount: {
+        currency: 'SPX',
+        value: String(amountSPX),
+        issuer: adminAccount[0].xrp_address_cold,
+      },
+      Destination: adminAccount[0].xrp_address_hot,
+    };
+    const payPrepared = await client.autofill(sendTokenTx);
+    const paySigned = sender.sign(payPrepared);
+    const payResult = await client.submitAndWait(paySigned.tx_blob);
+    if (payResult.result.meta.TransactionResult == 'tesSUCCESS') {
+      console.log(`Transaction succeeded: https://testnet.xrpl.org/transactions/${paySigned.hash}`);
+    } else {
+      // eslint-disable-next-line no-throw-literal
+      throw `Error sending transaction: ${payResult.result.meta.TransactionResult}`;
+    }
+  }
+  if (amountUPX > 0) {
+    const sendTokenTx = {
+      TransactionType: 'Payment',
+      Account: sender.address,
+      Amount: {
+        currency: 'UPX',
+        value: String(amountUPX),
+        issuer: adminAccount[0].xrp_address_cold,
+      },
+      Destination: adminAccount[0].xrp_address_hot,
+    };
+    const payPrepared = await client.autofill(sendTokenTx);
+    const paySigned = sender.sign(payPrepared);
+    const payResult = await client.submitAndWait(paySigned.tx_blob);
+    if (payResult.result.meta.TransactionResult == 'tesSUCCESS') {
+      console.log(`Transaction succeeded: https://testnet.xrpl.org/transactions/${paySigned.hash}`);
+    } else {
+      // eslint-disable-next-line no-throw-literal
+      throw `Error sending transaction: ${payResult.result.meta.TransactionResult}`;
+    }
+  }
+  client.disconnect();
 });
