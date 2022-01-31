d7e5d7249bba94522c9c9e6d3a3adc73e7f79646
Senna46 <29295263+Senna46@users.noreply.github.com>
2022-01-31 16:00:00 +0900

    fix: 残高不足の処理を追記

--- functions/src/balances/daily-usage.ts
+++ functions/src/balances/daily-usage.ts
@@ -5,7 +5,9 @@ import { balance } from '.';
 import { account_private } from '../account-privates';
 import { admin_account } from '../admin-accounts';
 import { daily_usage } from '../daily-usages';
+import { insufficient_balance } from '../insufficient-balances';
 import { student_account } from '../student-accounts';
+import { InsufficientBalance } from '@local/common';
 
 daily_usage.onCreateHandler.push(async (snapshot, context) => {
   const data = snapshot.data()!;
@@ -16,14 +18,19 @@ daily_usage.onCreateHandler.push(async (snapshot, context) => {
     console.log(data.room_id, 'no student');
     return;
   }
+  if (usage <= 0) {
+    console.log('0 or minus usage detected');
+    return;
+  }
   const accountBalance = await balance.getLatest(studentAccount[0].id);
+  const totalBalance = accountBalance[0].amount_spx + accountBalance[0].amount_upx;
   const accountPrivate = await account_private.list(studentAccount[0].id);
   const xrpl = require('xrpl');
   const TEST_NET = 'wss://s.altnet.rippletest.net:51233';
   const client = new xrpl.Client(TEST_NET);
   const adminAccount = await admin_account.getByName('admin');
 
-  if (usage < accountBalance[0].amount_spx) {
+  if (usage <= accountBalance[0].amount_spx) {
     await balance.update({
       id: accountBalance[0].id,
       student_account_id: accountBalance[0].student_account_id,
@@ -56,7 +63,7 @@ daily_usage.onCreateHandler.push(async (snapshot, context) => {
       throw `Error sending transaction: ${payResult.result.meta.TransactionResult}`;
     }
     client.disconnect();
-  } else {
+  } else if (usage < totalBalance) {
     const spxShortage = usage - accountBalance[0].amount_spx;
     await balance.update({
       id: accountBalance[0].id,
@@ -70,24 +77,26 @@ daily_usage.onCreateHandler.push(async (snapshot, context) => {
     }
     await client.connect();
     const sender = xrpl.Wallet.fromSeed(accountPrivate[0].xrp_seed);
-    const sendSPXTx = {
-      TransactionType: 'Payment',
-      Account: sender.address,
-      Amount: {
-        currency: 'SPX',
-        value: String(accountBalance[0].amount_spx),
-        issuer: adminAccount[0].xrp_address_cold,
-      },
-      Destination: adminAccount[0].xrp_address_hot,
-    };
-    const payPreparedSPX = await client.autofill(sendSPXTx);
-    const paySignedSPX = sender.sign(payPreparedSPX);
-    const payResultSPX = await client.submitAndWait(paySignedSPX.tx_blob);
-    if (payResultSPX.result.meta.TransactionResult == 'tesSUCCESS') {
-      console.log(`Transaction succeeded: https://testnet.xrpl.org/transactions/${paySignedSPX.hash}`);
-    } else {
-      // eslint-disable-next-line no-throw-literal
-      throw `Error sending transaction: ${payResultSPX.result.meta.TransactionResult}`;
+    if (accountBalance[0].amount_spx > 0) {
+      const sendSPXTx = {
+        TransactionType: 'Payment',
+        Account: sender.address,
+        Amount: {
+          currency: 'SPX',
+          value: String(accountBalance[0].amount_spx),
+          issuer: adminAccount[0].xrp_address_cold,
+        },
+        Destination: adminAccount[0].xrp_address_hot,
+      };
+      const payPreparedSPX = await client.autofill(sendSPXTx);
+      const paySignedSPX = sender.sign(payPreparedSPX);
+      const payResultSPX = await client.submitAndWait(paySignedSPX.tx_blob);
+      if (payResultSPX.result.meta.TransactionResult == 'tesSUCCESS') {
+        console.log(`Transaction succeeded: https://testnet.xrpl.org/transactions/${paySignedSPX.hash}`);
+      } else {
+        // eslint-disable-next-line no-throw-literal
+        throw `Error sending transaction: ${payResultSPX.result.meta.TransactionResult}`;
+      }
     }
     const sendUPXTx = {
       TransactionType: 'Payment',
@@ -107,6 +116,67 @@ daily_usage.onCreateHandler.push(async (snapshot, context) => {
     } else {
       // eslint-disable-next-line no-throw-literal
       throw `Error sending transaction: ${payResultUPX.result.meta.TransactionResult}`;
+    }
+    client.disconnect();
+  } else {
+    const insufficiency = usage - totalBalance;
+    await balance.update({
+      id: accountBalance[0].id,
+      student_account_id: accountBalance[0].student_account_id,
+      amount_upx: 0,
+      amount_spx: 0,
+    });
+    await insufficient_balance.create(
+      new InsufficientBalance({ student_account_id: accountBalance[0].student_account_id, amount: insufficiency }),
+    );
+
+    if (!accountPrivate.length) {
+      console.log(studentAccount[0].id, 'no XRP address');
+      return;
+    }
+    await client.connect();
+    const sender = xrpl.Wallet.fromSeed(accountPrivate[0].xrp_seed);
+    if (accountBalance[0].amount_spx > 0) {
+      const sendSPXTx = {
+        TransactionType: 'Payment',
+        Account: sender.address,
+        Amount: {
+          currency: 'SPX',
+          value: String(accountBalance[0].amount_spx),
+          issuer: adminAccount[0].xrp_address_cold,
+        },
+        Destination: adminAccount[0].xrp_address_hot,
+      };
+      const payPreparedSPX = await client.autofill(sendSPXTx);
+      const paySignedSPX = sender.sign(payPreparedSPX);
+      const payResultSPX = await client.submitAndWait(paySignedSPX.tx_blob);
+      if (payResultSPX.result.meta.TransactionResult == 'tesSUCCESS') {
+        console.log(`Transaction succeeded: https://testnet.xrpl.org/transactions/${paySignedSPX.hash}`);
+      } else {
+        // eslint-disable-next-line no-throw-literal
+        throw `Error sending transaction: ${payResultSPX.result.meta.TransactionResult}`;
+      }
+    }
+    if (accountBalance[0].amount_upx > 0) {
+      const sendUPXTx = {
+        TransactionType: 'Payment',
+        Account: sender.address,
+        Amount: {
+          currency: 'UPX',
+          value: String(accountBalance[0].amount_upx),
+          issuer: adminAccount[0].xrp_address_cold,
+        },
+        Destination: adminAccount[0].xrp_address_hot,
+      };
+      const payPreparedUPX = await client.autofill(sendUPXTx);
+      const paySignedUPX = sender.sign(payPreparedUPX);
+      const payResultUPX = await client.submitAndWait(paySignedUPX.tx_blob);
+      if (payResultUPX.result.meta.TransactionResult == 'tesSUCCESS') {
+        console.log(`Transaction succeeded: https://testnet.xrpl.org/transactions/${paySignedUPX.hash}`);
+      } else {
+        // eslint-disable-next-line no-throw-literal
+        throw `Error sending transaction: ${payResultUPX.result.meta.TransactionResult}`;
+      }
     }
     client.disconnect();
   }
