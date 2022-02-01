178382e0e8cc492373f898a98b243a41485b5e81
Senna46 <29295263+Senna46@users.noreply.github.com>
2022-02-01 14:45:10 +0900

    feat: 定期実行の変更

--- functions/src/single-price-renewable-settlements/renewable-contract.ts
+++ functions/src/single-price-renewable-settlements/renewable-contract.ts
@@ -10,7 +10,8 @@ import * as functions from 'firebase-functions';
 
 const f = functions.region('asia-northeast1');
 module.exports.renewableContract = f.pubsub
-  .schedule('0 0 * * *')
+  .schedule('every 10 minutes')
+  // .schedule('0 0 * * *')
   .timeZone('Asia/Tokyo') // Users can choose timezone - default is America/Los_Angeles
   .onRun(async () => {
     const renewableBids = await renewable_bid.listValid();
