99f969e7e67d4869789590fd6a75f0f8461600a4
Senna46 <29295263+Senna46@users.noreply.github.com>
2022-02-04 08:45:51 +0900

    feat: 自動生成コンポーネントたち

--- projects/main/src/app/page/chats/chats.component.ts
+++ projects/main/src/app/page/chats/chats.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { Auth, authState } from '@angular/fire/auth';
+import { ActivatedRoute } from '@angular/router';
+import { Chat, StudentAccount } from '@local/common';
+import { ChatApplicationService } from 'projects/shared/src/lib/services/chats/chat.application.service';
+import { StudentAccountApplicationService } from 'projects/shared/src/lib/services/student-accounts/student-account.application.service';
+import { Observable } from 'rxjs';
+import { mergeMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-chats',
   templateUrl: './chats.component.html',
-  styleUrls: ['./chats.component.css']
+  styleUrls: ['./chats.component.css'],
 })
 export class ChatsComponent implements OnInit {
+  studentAccount$: Observable<StudentAccount> | undefined;
+  chats$: Observable<Chat[]> | undefined;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private auth: Auth,
+    private route: ActivatedRoute,
+    private readonly studentAccApp: StudentAccountApplicationService,
+    private readonly chatApp: ChatApplicationService,
+  ) {
+    const user$ = authState(this.auth);
+    this.studentAccount$ = user$.pipe(mergeMap((user) => this.studentAccApp.getByUid$(user?.uid!)));
+    this.chats$ = this.studentAccount$.pipe(mergeMap((account) => this.chatApp.list$(account.id)));
   }
 
+  ngOnInit(): void {}
 }
--- projects/main/src/app/page/chats/chats.module.ts
+++ projects/main/src/app/page/chats/chats.module.ts
@@ -4,9 +4,11 @@ import { ChatsRoutingModule } from './chats-routing.module';
 import { ChatsComponent } from './chats.component';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { CreateComponent } from './create/create.component';
+import { MessageComponent } from './chat/message/message.component';
 
 @NgModule({
-  declarations: [ChatsComponent, ChatComponent],
+  declarations: [ChatsComponent, ChatComponent, CreateComponent, MessageComponent],
   imports: [CommonModule, ChatsRoutingModule, ChatsModule],
 })
 export class AppChatsModule {}
