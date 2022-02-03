846ce812e113a9261a236d812e8b9925efeba658
Senna46 <29295263+Senna46@users.noreply.github.com>
2022-02-04 08:11:34 +0900

    feat: function-domain(自動)

---
+++ functions/src/chats/module.ts
@@ -0,0 +1,66 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+
+/* eslint-disable require-jsdoc */
+import * as admin from 'firebase-admin';
+import { Chat, ChatFirestore } from '@local/common';
+
+export * from './controller'
+
+export function collection() {
+  return admin
+    .firestore()
+    .collection(ChatFirestore.collectionPath())
+    .withConverter(ChatFirestore.converter as any);
+}
+
+export function collectionGroup() {
+  return admin
+    .firestore()
+    .collectionGroup(ChatFirestore.collectionID)
+    .withConverter(ChatFirestore.converter as any);
+}
+
+export function document(id?: string) {
+  const col = collection();
+  return id ? col.doc(id) : col.doc();
+}
+
+export async function get(id: string) {
+  return await document(id)
+    .get()
+    .then((snapshot) => snapshot.data() as Chat);
+}
+
+export async function list() {
+  return await collection()
+    .get()
+    .then((snapshot) => snapshot.docs.map((doc) => doc.data() as Chat));
+}
+
+export async function create(
+  data: Chat
+) {
+  const doc = document();
+  data.id = doc.id;
+
+  const now = admin.firestore.Timestamp.now();
+  data.created_at = now;
+  data.updated_at = now;
+
+  await doc.set(data);
+}
+
+export async function update(
+  data: Partial<Chat> & { id: string }
+) {
+  const now = admin.firestore.Timestamp.now();
+  data.updated_at = now;
+
+  await document(data.id).update(data);
+}
+
+export async function delete_(id: string) {
+  await document(id).delete();
+}
