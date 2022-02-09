06816c91d824b32d4e90661f7675d4e75421d3b1
Senna Ohmura <29295263+Senna46@users.noreply.github.com>
2022-02-09 15:29:51 +0900

    feat: admin xrpl

---
+++ projects/main/src/app/view/admin/xrpl/xrpl.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'view-xrpl',
+  templateUrl: './xrpl.component.html',
+  styleUrls: ['./xrpl.component.css'],
+})
+export class XrplComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
