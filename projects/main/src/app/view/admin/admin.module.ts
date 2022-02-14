63c954e93efdfbb56def688c28cc5b9f11562868
Senna Ohmura <29295263+Senna46@users.noreply.github.com>
2022-02-14 20:20:32 +0900

    feat: function menu

--- projects/main/src/app/view/admin/admin.module.ts
+++ projects/main/src/app/view/admin/admin.module.ts
@@ -1,10 +1,13 @@
+import { MaterialModule } from '../../material.module';
 import { AdminComponent } from './admin.component';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [AdminComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule, FormsModule, MaterialModule],
   exports: [AdminComponent],
 })
 export class AdminModule {}
