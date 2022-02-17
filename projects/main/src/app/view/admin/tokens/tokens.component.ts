ffd4bd7cc92c6e76d58845ef4929cbb62ebaca25
Senna46 <29295263+Senna46@users.noreply.github.com>
2022-02-17 15:00:27 +0900

    feat: angular tokens

--- projects/main/src/app/view/admin/tokens/tokens.component.ts
+++ projects/main/src/app/view/admin/tokens/tokens.component.ts
@@ -1,15 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { NormalAskSetting, RenewableAskSetting } from '@local/common';
+
+export type SetNormalOnSubmitEvent = {
+  price: number;
+  amount: number;
+};
+
+export type SetRenewableOnSubmitEvent = {
+  price: number;
+  amount: number;
+};
 
 @Component({
-  selector: 'app-tokens',
+  selector: 'view-tokens',
   templateUrl: './tokens.component.html',
-  styleUrls: ['./tokens.component.css']
+  styleUrls: ['./tokens.component.css'],
 })
 export class TokensComponent implements OnInit {
+  @Input()
+  normalSetting?: NormalAskSetting | null;
+  @Input()
+  renewableSetting?: RenewableAskSetting | null;
+  @Output()
+  appSubmitNormal: EventEmitter<SetNormalOnSubmitEvent>;
+  @Output()
+  appSubmitRenewable: EventEmitter<SetRenewableOnSubmitEvent>;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.appSubmitNormal = new EventEmitter();
+    this.appSubmitRenewable = new EventEmitter();
   }
 
+  ngOnInit(): void {}
+
+  onSubmitNormal(price: string, amount: string) {
+    if (!price) {
+      alert('価格を設定してください');
+      return;
+    }
+    this.appSubmitNormal.emit({ price: Number(price), amount: Number(amount) });
+  }
+
+  onSubmitRenewable(price: string, amount: string) {
+    if (!price) {
+      alert('価格を設定してください');
+      return;
+    }
+    if (!amount) {
+      alert('発行量を設定してください');
+      return;
+    }
+    this.appSubmitRenewable.emit({ price: Number(price), amount: Number(amount) });
+  }
 }
--- projects/main/src/app/view/admin/tokens/tokens.module.ts
+++ projects/main/src/app/view/admin/tokens/tokens.module.ts
@@ -1,15 +1,13 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { MaterialModule } from '../../../material.module';
 import { TokensComponent } from './tokens.component';
-
-
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [
-    TokensComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [TokensComponent],
+  imports: [CommonModule, RouterModule, FormsModule, MaterialModule],
+  exports: [TokensComponent],
 })
-export class TokensModule { }
+export class TokensModule {}
