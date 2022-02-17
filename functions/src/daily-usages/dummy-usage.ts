2dc1f04b84242408eae99e074fd96dd470be6d4e
Senna46 <29295263+Senna46@users.noreply.github.com>
2022-02-17 20:58:25 +0900

    feat: ダミー消費電力の部屋を作成

--- functions/src/daily-usages/dummy-usage.ts
+++ functions/src/daily-usages/dummy-usage.ts
@@ -5,8 +5,10 @@ import * as functions from 'firebase-functions';
 
 const f = functions.region('asia-northeast1');
 module.exports.primaryRenewableAsk = f.pubsub
-  .schedule('30 9 * * *')
+  .schedule('0 12 * * *')
   .timeZone('Asia/Tokyo') // Users can choose timezone - default is America/Los_Angeles
   .onRun(async () => {
-    await daily_usage.create(new DailyUsage({ room_id: 'higashi999', amount_kwh_str: '5' }));
+    await daily_usage.create(new DailyUsage({ room_id: 'higashi101', amount_kwh_str: '10' }));
+    await daily_usage.create(new DailyUsage({ room_id: 'higashi102', amount_kwh_str: '20' }));
+    await daily_usage.create(new DailyUsage({ room_id: 'higashi103', amount_kwh_str: '30' }));
   });
