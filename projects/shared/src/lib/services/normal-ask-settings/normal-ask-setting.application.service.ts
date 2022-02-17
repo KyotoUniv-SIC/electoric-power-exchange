38fca4803a57ea17bb13e347bef8ebac00347667
Senna46 <29295263+Senna46@users.noreply.github.com>
2022-02-17 13:56:00 +0900

    feat: app service

--- projects/shared/src/lib/services/normal-ask-settings/normal-ask-setting.application.service.ts
+++ projects/shared/src/lib/services/normal-ask-settings/normal-ask-setting.application.service.ts
@@ -1,11 +1,71 @@
-import { Injectable } from '@angular/core';
 import { NormalAskSettingService } from './normal-ask-setting.service';
+import { Injectable } from '@angular/core';
+import { Timestamp } from '@angular/fire/firestore';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 import { NormalAskSetting } from '@local/common';
+import { LoadingDialogService } from 'ng-loading-dialog';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class NormalAskSettingApplicationService {
-  
-  constructor(private readonly normalAskSetting: NormalAskSettingService) {}
+  constructor(
+    private readonly normalAskSetting: NormalAskSettingService,
+    private readonly loadingDialog: LoadingDialogService,
+    private readonly snackBar: MatSnackBar,
+    private readonly router: Router,
+  ) {}
+
+  async create(data: NormalAskSetting) {
+    const dialogRef = this.loadingDialog.open('Sending New Setting');
+    try {
+      this.normalAskSetting.create(data);
+    } catch {
+      this.snackBar.open('Error has occurred', undefined, {
+        duration: 6000,
+      });
+      return;
+    } finally {
+      dialogRef.close();
+    }
+
+    this.snackBar.open('Successfully Update Setting', undefined, {
+      duration: 6000,
+    });
+
+    await this.router.navigate(['admin/tokens']);
+  }
+
+  get$(id: string) {
+    return this.normalAskSetting.get$(id);
+  }
+
+  getLatest$() {
+    return this.normalAskSetting.list$().pipe(
+      map(
+        (settings) =>
+          settings.sort(function (first, second) {
+            if (!first.created_at) {
+              return 1;
+            } else if (!second.created_at) {
+              return -1;
+            } else {
+              if ((first.created_at as Timestamp).toDate() > (second.created_at as Timestamp).toDate()) {
+                return -1;
+              } else if ((first.created_at as Timestamp).toDate() < (second.created_at as Timestamp).toDate()) {
+                return 1;
+              } else {
+                return 0;
+              }
+            }
+          })[0],
+      ),
+    );
+  }
+
+  list$() {
+    return this.normalAskSetting.list$();
+  }
 }
--- projects/shared/src/lib/services/renewable-ask-settings/renewable-ask-setting.application.service.ts
+++ projects/shared/src/lib/services/renewable-ask-settings/renewable-ask-setting.application.service.ts
@@ -1,11 +1,71 @@
-import { Injectable } from '@angular/core';
 import { RenewableAskSettingService } from './renewable-ask-setting.service';
+import { Injectable } from '@angular/core';
+import { Timestamp } from '@angular/fire/firestore';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 import { RenewableAskSetting } from '@local/common';
+import { LoadingDialogService } from 'ng-loading-dialog';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class RenewableAskSettingApplicationService {
-  
-  constructor(private readonly renewableAskSetting: RenewableAskSettingService) {}
+  constructor(
+    private readonly renewableAskSetting: RenewableAskSettingService,
+    private readonly loadingDialog: LoadingDialogService,
+    private readonly snackBar: MatSnackBar,
+    private readonly router: Router,
+  ) {}
+
+  async create(data: RenewableAskSetting) {
+    const dialogRef = this.loadingDialog.open('Sending New Setting');
+    try {
+      this.renewableAskSetting.create(data);
+    } catch {
+      this.snackBar.open('Error has occurred', undefined, {
+        duration: 6000,
+      });
+      return;
+    } finally {
+      dialogRef.close();
+    }
+
+    this.snackBar.open('Successfully Update Setting', undefined, {
+      duration: 6000,
+    });
+
+    await this.router.navigate(['admin/tokens']);
+  }
+
+  get$(id: string) {
+    return this.renewableAskSetting.get$(id);
+  }
+
+  getLatest$() {
+    return this.renewableAskSetting.list$().pipe(
+      map(
+        (settings) =>
+          settings.sort(function (first, second) {
+            if (!first.created_at) {
+              return 1;
+            } else if (!second.created_at) {
+              return -1;
+            } else {
+              if ((first.created_at as Timestamp).toDate() > (second.created_at as Timestamp).toDate()) {
+                return -1;
+              } else if ((first.created_at as Timestamp).toDate() < (second.created_at as Timestamp).toDate()) {
+                return 1;
+              } else {
+                return 0;
+              }
+            }
+          })[0],
+      ),
+    );
+  }
+
+  list$() {
+    return this.renewableAskSetting.list$();
+  }
 }
