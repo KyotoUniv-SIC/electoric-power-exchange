f475985416aefa75af4415b9c634a6885601682b
Senna46 <29295263+Senna46@users.noreply.github.com>
2022-02-17 17:28:36 +0900

    feat: 部屋の変更を即時反映に

--- functions/src/student-accounts/room-change.ts
+++ functions/src/student-accounts/room-change.ts
@@ -4,9 +4,10 @@ import { room_change } from '../room-changes';
 
 room_change.onCreateHandler.push(async (snapshot, context) => {
   const data = snapshot.data()!;
-  if (!data.room_id_before) {
-    await student_account.update({ id: data.student_account_id, room_id: data.room_id_after });
-  } else {
-    console.error(data.student_account_id, 'change room request detected');
-  }
+  await student_account.update({ id: data.student_account_id, room_id: data.room_id_after });
+  // if (!data.room_id_before) {
+  //   await student_account.update({ id: data.student_account_id, room_id: data.room_id_after });
+  // } else {
+  //   console.error(data.student_account_id, 'change room request detected');
+  // }
 });
