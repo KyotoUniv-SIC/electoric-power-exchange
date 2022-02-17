fb468275a0f27a3b469ab79ac90a6e53dbef3ca4
Senna46 <29295263+Senna46@users.noreply.github.com>
2022-02-17 20:59:40 +0900

    feat: 20upx付与に変更

--- functions/src/student-accounts/xrpl.ts
+++ functions/src/student-accounts/xrpl.ts
@@ -7,7 +7,8 @@ import { student_account } from '.';
 import { account_private } from '../account-privates';
 import { admin_account } from '../admin-accounts';
 import { admin_private } from '../admin-privates';
-import { AccountPrivate } from '@local/common';
+import { primary_ask } from '../primary-asks';
+import { AccountPrivate, PrimaryAsk } from '@local/common';
 
 student_account.onCreateHandler.push(async (snapshot, context) => {
   const data = snapshot.data()!;
@@ -66,33 +67,33 @@ student_account.onCreateHandler.push(async (snapshot, context) => {
       throw `Error sending transaction: ${tsResultRenewable.result.meta.TransactionResult}`;
     }
 
-    // アカウント作成時、試験的に50トークンずつ付与する仕様に（精算には含めていない）
+    // アカウント作成時、20UPXトークン付与する仕様に（精算には含めていない）
     const sender = xrpl.Wallet.fromSeed(adminPrivate[0].xrp_seed_hot);
-    const sendSPXTx = {
-      TransactionType: 'Payment',
-      Account: sender.address,
-      Amount: {
-        currency: 'SPX',
-        value: String(50),
-        issuer: adminAccount[0].xrp_address_cold,
-      },
-      Destination: wallet.address,
-    };
-    const payPreparedSPX = await client.autofill(sendSPXTx);
-    const paySignedSPX = sender.sign(payPreparedSPX);
-    const payResultSPX = await client.submitAndWait(paySignedSPX.tx_blob);
-    if (payResultSPX.result.meta.TransactionResult == 'tesSUCCESS') {
-      console.log(`Transaction succeeded: https://testnet.xrpl.org/transactions/${paySignedSPX.hash}`);
-    } else {
-      // eslint-disable-next-line no-throw-literal
-      throw `Error sending transaction: ${payResultSPX.result.meta.TransactionResult}`;
-    }
+    // const sendSPXTx = {
+    //   TransactionType: 'Payment',
+    //   Account: sender.address,
+    //   Amount: {
+    //     currency: 'SPX',
+    //     value: String(50),
+    //     issuer: adminAccount[0].xrp_address_cold,
+    //   },
+    //   Destination: wallet.address,
+    // };
+    // const payPreparedSPX = await client.autofill(sendSPXTx);
+    // const paySignedSPX = sender.sign(payPreparedSPX);
+    // const payResultSPX = await client.submitAndWait(paySignedSPX.tx_blob);
+    // if (payResultSPX.result.meta.TransactionResult == 'tesSUCCESS') {
+    //   console.log(`Transaction succeeded: https://testnet.xrpl.org/transactions/${paySignedSPX.hash}`);
+    // } else {
+    //   // eslint-disable-next-line no-throw-literal
+    //   throw `Error sending transaction: ${payResultSPX.result.meta.TransactionResult}`;
+    // }
     const sendUPXTx = {
       TransactionType: 'Payment',
       Account: sender.address,
       Amount: {
         currency: 'UPX',
-        value: String(50),
+        value: String(20),
         issuer: adminAccount[0].xrp_address_cold,
       },
       Destination: wallet.address,
@@ -115,4 +116,5 @@ student_account.onCreateHandler.push(async (snapshot, context) => {
   await account_private.create(
     new AccountPrivate({ student_account_id: data.id, xrp_private_key: wallet.privateKey, xrp_seed: wallet.seed }),
   );
+  await primary_ask.create(new PrimaryAsk({ account_id: data.id, price: 27, amount: 20 }));
 });
