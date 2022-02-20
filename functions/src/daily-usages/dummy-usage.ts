71b06b14d19ee9bd7c2be89a22d6b6a69e9f5f1c
Senna46 <29295263+Senna46@users.noreply.github.com>
2022-02-20 17:55:13 +0900

    fix: ダミーの使用量引き落とし時刻の変更

--- functions/src/daily-usages/dummy-usage.ts
+++ functions/src/daily-usages/dummy-usage.ts
@@ -5,7 +5,7 @@ import * as functions from 'firebase-functions';
 
 const f = functions.region('asia-northeast1');
 module.exports.primaryRenewableAsk = f.pubsub
-  .schedule('0 12 * * *')
+  .schedule('0 10 * * *')
   .timeZone('Asia/Tokyo') // Users can choose timezone - default is America/Los_Angeles
   .onRun(async () => {
     await daily_usage.create(new DailyUsage({ room_id: 'higashi101', amount_kwh_str: '5' }));
