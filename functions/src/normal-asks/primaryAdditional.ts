21b4b72d419e49145ca9e51c16014ec619c46c74
Senna Ohmura <29295263+Senna46@users.noreply.github.com>
2022-02-22 16:59:36 +0900

    fix: primary additional

--- functions/src/normal-asks/primaryAdditional.ts
+++ functions/src/normal-asks/primaryAdditional.ts
@@ -1,7 +1,6 @@
 /* eslint-disable camelcase */
 import { normal_ask } from '.';
 import { admin_account } from '../admin-accounts';
-import { balance } from '../balances';
 import { insufficient_balance } from '../insufficient-balances';
 import { normal_ask_setting } from '../normal-ask-settings';
 import { student_account } from '../student-accounts';
@@ -9,49 +8,71 @@ import { NormalAsk, NormalAskSetting, proto } from '@local/common';
 import * as functions from 'firebase-functions';
 
 const f = functions.region('asia-northeast1');
-module.exports.primaryRenewableAsk = f.pubsub
-  .schedule('0 12 * * *')
+module.exports.primaryNormalAsk = f.pubsub
+  .schedule('0 17 * * *')
   .timeZone('Asia/Tokyo') // Users can choose timezone - default is America/Los_Angeles
   .onRun(async () => {
     const setting = await normal_ask_setting.getLatest();
     const adminAccount = await admin_account.getByName('admin');
     const students = await student_account.list();
     if (!setting) {
-      let amountUPX = 0;
+      // const amountUPX = 0;
       let amountInsufficient = 0;
       for (const student of students) {
-        amountUPX += (await balance.list(student.id))[0].amount_upx;
+        // amountUPX += (await balance.list(student.id))[0].amount_upx;
         for (const insufficient of await insufficient_balance.list(student.id)) {
           amountInsufficient += insufficient.amount;
         }
       }
-      if (amountUPX < amountInsufficient) {
+      // if (amountUPX < amountInsufficient) {
+      //   await normal_ask.create(
+      //     new NormalAsk({
+      //       type: proto.main.NormalAskType.SECONDARY,
+      //       account_id: adminAccount[0].id,
+      //       price: 27,
+      //       amount: amountInsufficient - amountUPX,
+      //       is_deleted: false,
+      //     }),
+      //   );
+      // }
+      if (amountInsufficient) {
         await normal_ask.create(
           new NormalAsk({
             type: proto.main.NormalAskType.SECONDARY,
             account_id: adminAccount[0].id,
             price: 27,
-            amount: amountInsufficient - amountUPX,
+            amount: amountInsufficient,
             is_deleted: false,
           }),
         );
       }
     } else if (!setting.amount) {
-      let amountUPX = 0;
+      // let amountUPX = 0;
       let amountInsufficient = 0;
       for (const student of students) {
-        amountUPX += (await balance.list(student.id))[0].amount_upx;
+        // amountUPX += (await balance.list(student.id))[0].amount_upx;
         for (const insufficient of await insufficient_balance.list(student.id)) {
           amountInsufficient += insufficient.amount;
         }
       }
-      if (amountUPX < amountInsufficient) {
+      // if (amountUPX < amountInsufficient) {
+      //   await normal_ask.create(
+      //     new NormalAsk({
+      //       type: proto.main.NormalAskType.SECONDARY,
+      //       account_id: adminAccount[0].id,
+      //       price: setting.price,
+      //       amount: amountInsufficient - amountUPX,
+      //       is_deleted: false,
+      //     }),
+      //   );
+      // }
+      if (amountInsufficient) {
         await normal_ask.create(
           new NormalAsk({
             type: proto.main.NormalAskType.SECONDARY,
             account_id: adminAccount[0].id,
             price: setting.price,
-            amount: amountInsufficient - amountUPX,
+            amount: amountInsufficient,
             is_deleted: false,
           }),
         );
