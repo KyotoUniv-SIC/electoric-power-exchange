1949ce1e0ed2cc81018eb9a8989e29066ec1cfda
Senna Ohmura <29295263+Senna46@users.noreply.github.com>
2022-03-03 15:16:38 +0900

    feat: オーダーの作成時刻を記録

--- common/src/entities/renewable-ask-histories/renewable-ask-history.ts
+++ common/src/entities/renewable-ask-histories/renewable-ask-history.ts
@@ -2,7 +2,12 @@ import { proto } from '../..';
 import { FieldValue, Timestamp } from 'firebase/firestore';
 
 export class RenewableAskHistory extends proto.main.RenewableAskHistory {
-  constructor(iRenewableAskHistory: proto.main.IRenewableAskHistory, public created_at?: FieldValue | Timestamp, public updated_at?: FieldValue | Timestamp) {
+  constructor(
+    iRenewableAskHistory: proto.main.IRenewableAskHistory,
+    public ask_created_at?: FieldValue | Timestamp,
+    public created_at?: FieldValue | Timestamp,
+    public updated_at?: FieldValue | Timestamp,
+  ) {
     super(iRenewableAskHistory);
   }
 
--- common/src/entities/renewable-bid-histories/renewable-bid-history.ts
+++ common/src/entities/renewable-bid-histories/renewable-bid-history.ts
@@ -2,7 +2,12 @@ import { proto } from '../..';
 import { FieldValue, Timestamp } from 'firebase/firestore';
 
 export class RenewableBidHistory extends proto.main.RenewableBidHistory {
-  constructor(iRenewableBidHistory: proto.main.IRenewableBidHistory, public created_at?: FieldValue | Timestamp, public updated_at?: FieldValue | Timestamp) {
+  constructor(
+    iRenewableBidHistory: proto.main.IRenewableBidHistory,
+    public bid_created_at?: FieldValue | Timestamp,
+    public created_at?: FieldValue | Timestamp,
+    public updated_at?: FieldValue | Timestamp,
+  ) {
     super(iRenewableBidHistory);
   }
 
