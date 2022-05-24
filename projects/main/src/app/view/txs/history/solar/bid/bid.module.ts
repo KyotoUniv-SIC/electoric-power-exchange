4281963acdcad7f6d604801c0fc5cdb03d477f44
Senna46 <29295263+Senna46@users.noreply.github.com>
2022-05-25 01:01:31 +0900

    fix: transaction detail page

--- projects/main/src/app/view/txs/history/solar/bid/bid.module.ts
+++ projects/main/src/app/view/txs/history/solar/bid/bid.module.ts
@@ -4,10 +4,11 @@ import { NgModule } from '@angular/core';
 import { MatChipsModule } from '@angular/material/chips';
 import { RouterModule } from '@angular/router';
 import { MaterialModule } from 'projects/main/src/app/material.module';
+import { PipesModule } from 'projects/main/src/app/pipes/pipes.module';
 
 @NgModule({
   declarations: [BidComponent],
-  imports: [CommonModule, RouterModule, MaterialModule, MatChipsModule],
+  imports: [CommonModule, RouterModule, MaterialModule, MatChipsModule,PipesModule],
   exports: [BidComponent],
 })
 export class BidModule {}
