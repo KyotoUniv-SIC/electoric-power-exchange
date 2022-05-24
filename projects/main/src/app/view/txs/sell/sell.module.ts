b04dfbfd7c7d0702b393fdd4398dcb472249ff13
Senna46 <29295263+Senna46@users.noreply.github.com>
2022-05-25 00:46:59 +0900

    fix: sell component

--- projects/main/src/app/view/txs/sell/sell.module.ts
+++ projects/main/src/app/view/txs/sell/sell.module.ts
@@ -1,4 +1,5 @@
 import { MaterialModule } from '../../../material.module';
+import { PipesModule } from '../../../pipes/pipes.module';
 import { SellComponent } from './sell.component';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
@@ -7,7 +8,7 @@ import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [SellComponent],
-  imports: [CommonModule, RouterModule, FormsModule, ReactiveFormsModule, MaterialModule],
+  imports: [CommonModule, RouterModule, FormsModule, ReactiveFormsModule, MaterialModule, PipesModule],
   exports: [SellComponent],
 })
 export class SellModule {}
