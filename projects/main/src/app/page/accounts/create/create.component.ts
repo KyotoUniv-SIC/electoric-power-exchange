824a44c141c8dd5740b3e9b7a420c095c37100c2
Senna Ohmura <29295263+Senna46@users.noreply.github.com>
2022-05-26 17:32:49 +0900

    feat: add room change

--- projects/main/src/app/page/accounts/create/create.component.ts
+++ projects/main/src/app/page/accounts/create/create.component.ts
@@ -1,6 +1,10 @@
 import { CreateOnSubmitEvent } from '../../../view/accounts/create/create.component';
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { RoomChange } from '@local/common';
 import { AuthApplicationService } from 'projects/shared/src/lib/services/auth/auth.application.service';
+import { RoomChangeApplicationService } from 'projects/shared/src/lib/services/room-changes/room-change.application.service';
+import { StudentAccountApplicationService } from 'projects/shared/src/lib/services/student-accounts/student-account.application.service';
 
 @Component({
   selector: 'app-create',
@@ -8,12 +12,17 @@ import { AuthApplicationService } from 'projects/shared/src/lib/services/auth/au
   styleUrls: ['./create.component.css'],
 })
 export class CreateComponent implements OnInit {
-  constructor(private auth: AuthApplicationService) {}
+  constructor(
+    private auth: AuthApplicationService,
+    private readonly studentAccApp: StudentAccountApplicationService,
+    private readonly roomChangeApp: RoomChangeApplicationService,
+    private snackBar: MatSnackBar,
+  ) {}
 
   ngOnInit(): void {}
 
   async onSubmit($event: CreateOnSubmitEvent) {
-    this.auth.signup(
+    const uid = await this.auth.signup(
       {
         type: 'email',
         name: $event.name,
@@ -22,5 +31,11 @@ export class CreateComponent implements OnInit {
       },
       $event.name,
     );
+    if (!uid) {
+      this.snackBar.open('Faied to create new account!', 'Close');
+      return;
+    }
+    const student = await this.studentAccApp.getByUid(uid);
+    await this.roomChangeApp.create(new RoomChange({ student_account_id: student.id, room_id_before: '', room_id_after: '' }));
   }
 }
