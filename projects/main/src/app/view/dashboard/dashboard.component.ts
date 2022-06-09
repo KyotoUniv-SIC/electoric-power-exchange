e10347bcc501448368a952ea7a1e8f227458c4e5
Ryo0731 <95175659+Ryo0731@users.noreply.github.com>
2022-06-09 17:37:31 +0900

    fix: datasource

--- projects/main/src/app/view/dashboard/dashboard.component.ts
+++ projects/main/src/app/view/dashboard/dashboard.component.ts
@@ -11,12 +11,7 @@ export interface PeriodicElement {
   charge: number;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  { classification: 'Minimum Charge', usage: 'Until the first 10 kWh', unit: 'Per Contract', charge: 341.01 },
-  { classification: 'Energy Charge', usage: 'Over 10 kWh up to 120 kWh', unit: '1kWh', charge: 20.31 },
-  { classification: 'Energy Charge', usage: 'Over 120 kWh up to 300 kWh', unit: '1kWh', charge: 25.71 },
-  { classification: 'Energy Charge', usage: 'Over 300 kWh', unit: '1kWh', charge: 28.7 },
-];
+
 
 @Component({
   selector: 'view-dashboard',
@@ -96,7 +91,12 @@ export class DashboardComponent implements OnInit {
   ];
 
   displayedColumns: string[] = ['classification', 'usage', 'unit', 'charge'];
-  dataSource = ELEMENT_DATA;
+  dataSource =  [
+    { classification: 'Minimum Charge', usage: 'Until the first 10 kWh', unit: 'Per Contract', charge: 341.01 },
+    { classification: 'Energy Charge', usage: 'Over 10 kWh up to 120 kWh', unit: '1kWh', charge: 20.31 },
+    { classification: 'Energy Charge', usage: 'Over 120 kWh up to 300 kWh', unit: '1kWh', charge: 25.71 },
+    { classification: 'Energy Charge', usage: 'Over 300 kWh', unit: '1kWh', charge: 28.7 },
+  ];;
 
   constructor() {}
 
