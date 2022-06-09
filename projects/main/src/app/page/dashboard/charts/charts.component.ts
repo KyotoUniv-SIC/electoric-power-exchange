c68ab6b6ea2eadc839781035ce33e52629d1c526
Ryo0731 <95175659+Ryo0731@users.noreply.github.com>
2022-06-09 21:32:21 +0900

    fix: charts

--- projects/main/src/app/page/dashboard/charts/charts.component.ts
+++ projects/main/src/app/page/dashboard/charts/charts.component.ts
@@ -221,8 +221,6 @@ export class ChartsComponent implements OnInit {
     this.renewableChartOptions$.subscribe((a) => console.log(a));
     this.normalChartDates$.subscribe((a) => console.log(a));
     this.renewableChartDates$.subscribe((a) => console.log(a));
-
-    this.normalDate$.subscribe((a) => console.log(a));
   }
 
   ngOnInit(): void {}
