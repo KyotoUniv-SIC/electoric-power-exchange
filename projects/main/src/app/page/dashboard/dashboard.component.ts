88459b7d5dbafa1b2cb721db5d5d753220d4bd99
Ryo0731 <95175659+Ryo0731@users.noreply.github.com>
2022-06-23 19:02:52 +0900

    feat: コード順番変更

--- projects/main/src/app/page/dashboard/dashboard.component.ts
+++ projects/main/src/app/page/dashboard/dashboard.component.ts
@@ -1,8 +1,6 @@
-import { DataSource } from '@angular/cdk/collections';
 import { Component, OnInit } from '@angular/core';
 import { Auth, authState } from '@angular/fire/auth';
 import { Timestamp } from '@angular/fire/firestore';
-import { ActivatedRoute } from '@angular/router';
 import {
   Balance,
   DailyUsage,
@@ -109,90 +107,14 @@ export class DashboardComponent implements OnInit {
     private readonly adminApp: AdminAccountApplicationService,
     private readonly dailyPaymentApp: DailyPaymentApplicationService,
   ) {
-    const now = new Date();
     let firstDay = new Date();
     firstDay.setUTCDate(1);
     firstDay.setUTCHours(0, 0, 0, 0);
     const currentUser$ = authState(this.auth);
     const studentAccount$ = currentUser$.pipe(mergeMap((user) => this.studentAccApp.getByUid$(user?.uid!)));
     const users$ = this.studentsApp.list$();
-    this.rankings$ = users$.pipe(
-      mergeMap((users) =>
-        Promise.all(
-          users.map((user) =>
-            this.dailyUsageApp.getRoom(user.room_id).then((usages) => {
-              let count = 0;
-              for (const usage of usages) {
-                (usage.created_at as Timestamp).toDate() > firstDay ? (count += parseInt(usage.amount_kwh_str)) : count;
-              }
-              return { id: user.id, name: user.name, kwhAmount: count };
-            }),
-          ),
-        ),
-      ),
-      // mapはforEachの機能に加えて新しい配列を返します（forEachは何も返さず、必ずvoidになる）
-      map((rankings) => {
-        let count = 1;
-        let tmp = 0;
-        // ここでランキングをソートして、順位をrankに入れる
-        let sortedRanking = rankings
-          .sort((first, second) => second.kwhAmount - first.kwhAmount)
-          .map((item, index) => {
-            if (item.kwhAmount !== tmp) {
-              count = index + 1;
-              tmp = item.kwhAmount;
-            }
-            // ここのreturnは86行目{}を受けてreturnしてます (85行目Array.map()の返り値)
-            return { id: item.id, rank: count, name: item.name, kwhAmount: item.kwhAmount };
-          });
-        //  ここのreturnは79行目{}を受けてreturnしてます (79行目Observable.map()の返り値)
-        return sortedRanking;
-      }),
-    );
-    this.rank$ = combineLatest([this.rankings$, studentAccount$]).pipe(
-      map(([rankings, account]) => rankings.find((ranking) => ranking.id == account.id)?.rank),
-    );
 
-    const co2Rankings$ = users$.pipe(
-      mergeMap((users) =>
-        Promise.all(
-          users.map((user) =>
-            this.dailyPaymentApp.list(user.id).then((payments) => {
-              let mwhCount = 0;
-              let uspxCount = 0;
-              for (const payment of payments) {
-                if ((payment.created_at as Timestamp).toDate() > firstDay) {
-                  mwhCount += parseInt(payment.amount_mwh);
-                  uspxCount += parseInt(payment.amount_uspx);
-                }
-              }
-              return { id: user.id, name: user.name, mwhAmount: mwhCount, uspxAmount: uspxCount };
-            }),
-          ),
-        ),
-      ),
-      map((co2Rankings) => {
-        let count = 1;
-        let tmp = 0;
-        let sortedCo2Ranking = co2Rankings
-          .sort((first, second) => second.uspxAmount / second.mwhAmount - first.uspxAmount / first.mwhAmount)
-          .map((item, index) => {
-            if (item.uspxAmount / item.mwhAmount != tmp) {
-              count = index + 1;
-              tmp = item.uspxAmount / item.mwhAmount;
-            }
-            return { id: item.id, rank: count, name: item.name, mwhAmount: item.mwhAmount, uspxAmount: item.uspxAmount };
-          });
-        return sortedCo2Ranking;
-      }),
-    );
-    this.co2Rank$ = combineLatest([co2Rankings$, studentAccount$]).pipe(
-      map(([rankings, account]) => {
-        const ranking = rankings.find((ranking) => ranking.id == account.id);
-        return { rank: ranking?.rank!, uspxPercentage: ranking?.uspxAmount! / ranking?.mwhAmount! };
-      }),
-    );
-
+    // 1.Account Balance
     const balance$ = studentAccount$.pipe(mergeMap((account) => this.balanceApp.getByUid$(account.id)));
     this.balanceData$ = balance$.pipe(
       map((balance) => [[parseInt(balance.amount_uupx) / 1000000, parseInt(balance.amount_uspx) / 1000000]]),
@@ -243,89 +165,7 @@ export class DashboardComponent implements OnInit {
       ),
     );
 
-    const usageListDaily$ = studentAccount$.pipe(mergeMap((account) => this.dailyUsageApp.getRoom$(account.room_id)));
-    const usageListDailyTotal$ = this.dailyUsageApp.list$();
-    const usageListMonthly$ = studentAccount$.pipe(mergeMap((account) => this.monthlyUsageApp.list$(account.id)));
-
-    this.latestUsage$ = usageListDaily$.pipe(
-      map(
-        (usages) =>
-          usages.sort(function (first, second) {
-            // 降順に並び替え
-            if ((first.created_at as Timestamp).toDate() > (second.created_at as Timestamp).toDate()) {
-              return -1;
-            } else if ((first.created_at as Timestamp).toDate() < (second.created_at as Timestamp).toDate()) {
-              return 1;
-            } else {
-              return 0;
-            }
-          })[0],
-      ),
-    );
-    this.withdrawDate$ = this.latestUsage$.pipe(
-      map((usage) => {
-        const createdAt = (usage.created_at as Timestamp).toDate();
-        createdAt.setDate(createdAt.getDate() + 1);
-        return createdAt;
-      }),
-    );
-    this.totalUsage$ = usageListDaily$.pipe(
-      map((usages) => {
-        let count = 0;
-        for (const usage of usages) {
-          (usage.created_at as Timestamp).toDate() > firstDay ? (count += parseInt(usage.amount_kwh_str)) : count;
-        }
-        return count;
-      }),
-    );
-
-    this.totalUsageAverage$ = usageListDailyTotal$.pipe(
-      map((usages) => {
-        let count = 0;
-        for (const usage of usages) {
-          (usage.created_at as Timestamp).toDate() > firstDay ? (count += parseInt(usage.amount_kwh_str)) : count;
-        }
-        // higashi-20, koushi-26, sentetsu-28
-        return (count / (20 + 26 + 28)).toFixed(2);
-      }),
-    );
-
-    const usages$ = combineLatest([this.totalUsage$, usageListMonthly$]).pipe(
-      map(([totalUsage, monthlyUsages]) => {
-        let data = monthlyUsages
-          .filter((usage) => parseInt(usage.year) == now.getFullYear())
-          .sort((first, second) => parseInt(first.month) - parseInt(second.month));
-
-        // .getMonth() 与えた日付の「月」を表す 0 から 11 までの間の整数値
-        let lackBefore = !data.length ? now.getMonth() : parseInt(data[0].month) - 1;
-        let lackAfter = !data.length ? 11 - now.getMonth() : 12 - parseInt(data[data.length - 1].month);
-        // 前月以前のデータに0を入れる
-        for (let i = 0; i < lackBefore; i++) {
-          data.unshift(new MonthlyUsage({ amount_mwh: '0' }));
-        }
-        // 今月のデータを追加
-        data.push(new MonthlyUsage({ amount_mwh: (totalUsage * 1000000).toString() }));
-        // 来月以降のデータに0を入れる
-        for (let i = 0; i < lackAfter; i++) {
-          data.push(new MonthlyUsage({ amount_mwh: '0' }));
-        }
-        return data.map((usage) => parseInt(usage.amount_mwh) / 1000000);
-      }),
-    );
-
-    const usagesPreviousYear$ = usageListMonthly$.pipe(
-      map((usages) => {
-        let data = usages
-          .filter((usage) => parseInt(usage.year) == now.getFullYear() - 1)
-          .sort((first, second) => parseInt(first.month) - parseInt(second.month))
-          .map((usage) => parseInt(usage.amount_mwh) / 1000000);
-        let lack = 12 - data.length;
-        for (let i = 0; i < lack; i++) {
-          data.unshift(0);
-        }
-        return data;
-      }),
-    );
+    // 2.Latest Contract Price
     this.normalSettlement$ = this.singlePriceNormalApp.getLatest$();
     this.normalDate$ = this.normalSettlement$.pipe(map((single) => (single.market_date as Timestamp).toDate()));
     this.renewableSettlement$ = this.singlePriceRenewableApp.getLatest$();
@@ -333,6 +173,7 @@ export class DashboardComponent implements OnInit {
     this.normalSettlements$ = this.singlePriceNormalApp.listLatestMonth$();
     const renewableSettlements$ = this.singlePriceRenewableApp.listLatestMonth$();
 
+    // 3.Contract Price & Amount Charts (4.は無し)
     const pricesNormal$ = this.normalSettlements$.pipe(map((params) => params.map((param) => parseInt(param.price_ujpy) / 1000000)));
     const amountsNormal$ = this.normalSettlements$.pipe(map((params) => params.map((param) => parseInt(param.amount_uupx) / 1000000)));
     const pricesRenewable$ = renewableSettlements$.pipe(map((params) => params.map((param) => parseInt(param.price_ujpy) / 1000000)));
@@ -504,10 +345,139 @@ export class DashboardComponent implements OnInit {
         };
       }),
     );
+
+    // 5.System Operation Status
     const adminAccounts$ = this.adminApp.getByName$('admin');
     this.normalOperationBids$ = adminAccounts$.pipe(mergeMap((adminAccount) => this.normalBidApp.listUid$(adminAccount[0].id)));
     this.normalOperationAsks$ = adminAccounts$.pipe(mergeMap((adminAccount) => this.normalAskApp.listUid$(adminAccount[0].id)));
     this.renewableOperationAsks$ = adminAccounts$.pipe(mergeMap((adminAccount) => this.renewableAskApp.listUid$(adminAccount[0].id)));
+
+    // 6.Next Withdrawal
+    const usageListDaily$ = studentAccount$.pipe(mergeMap((account) => this.dailyUsageApp.getRoom$(account.room_id)));
+    const usageListDailyTotal$ = this.dailyUsageApp.list$();
+    const usageListMonthly$ = studentAccount$.pipe(mergeMap((account) => this.monthlyUsageApp.list$(account.id)));
+    this.latestUsage$ = usageListDaily$.pipe(
+      map(
+        (usages) =>
+          usages.sort((first, second) => {
+            // 降順に並び替え
+            if ((first.created_at as Timestamp).toDate() > (second.created_at as Timestamp).toDate()) {
+              return -1;
+            } else if ((first.created_at as Timestamp).toDate() < (second.created_at as Timestamp).toDate()) {
+              return 1;
+            } else {
+              return 0;
+            }
+          })[0],
+      ),
+    );
+    this.withdrawDate$ = this.latestUsage$.pipe(
+      map((usage) => {
+        const createdAt = (usage.created_at as Timestamp).toDate();
+        createdAt.setDate(createdAt.getDate() + 1);
+        return createdAt;
+      }),
+    );
+
+    // 7.ランキング表示
+    this.rankings$ = users$.pipe(
+      mergeMap((users) =>
+        Promise.all(
+          users.map((user) =>
+            this.dailyUsageApp.getRoom(user.room_id).then((usages) => {
+              let count = 0;
+              for (const usage of usages) {
+                (usage.created_at as Timestamp).toDate() > firstDay ? (count += parseInt(usage.amount_kwh_str)) : count;
+              }
+              return { id: user.id, name: user.name, kwhAmount: count };
+            }),
+          ),
+        ),
+      ),
+      // mapはforEachの機能に加えて新しい配列を返します（forEachは何も返さず、必ずvoidになる）
+      map((rankings) => {
+        let count = 1;
+        let tmp = 0;
+        // ここでランキングをソートして、順位をrankに入れる
+        let sortedRanking = rankings
+          .sort((first, second) => second.kwhAmount - first.kwhAmount)
+          .map((item, index) => {
+            if (item.kwhAmount !== tmp) {
+              count = index + 1;
+              tmp = item.kwhAmount;
+            }
+            // ここのreturnは86行目{}を受けてreturnしてます (85行目Array.map()の返り値)
+            return { id: item.id, rank: count, name: item.name, kwhAmount: item.kwhAmount };
+          });
+        //  ここのreturnは79行目{}を受けてreturnしてます (79行目Observable.map()の返り値)
+        return sortedRanking;
+      }),
+    );
+    this.rank$ = combineLatest([this.rankings$, studentAccount$]).pipe(
+      map(([rankings, account]) => rankings.find((ranking) => ranking.id == account.id)?.rank),
+    );
+
+    const co2Rankings$ = users$.pipe(
+      mergeMap((users) =>
+        Promise.all(
+          users.map((user) =>
+            this.dailyPaymentApp.list(user.id).then((payments) => {
+              let mwhCount = 0;
+              let uspxCount = 0;
+              for (const payment of payments) {
+                if ((payment.created_at as Timestamp).toDate() > firstDay) {
+                  mwhCount += parseInt(payment.amount_mwh);
+                  uspxCount += parseInt(payment.amount_uspx);
+                }
+              }
+              return { id: user.id, name: user.name, mwhAmount: mwhCount, uspxAmount: uspxCount };
+            }),
+          ),
+        ),
+      ),
+      map((co2Rankings) => {
+        let count = 1;
+        let tmp = 0;
+        let sortedCo2Ranking = co2Rankings
+          .sort((first, second) => second.uspxAmount / second.mwhAmount - first.uspxAmount / first.mwhAmount)
+          .map((item, index) => {
+            if (item.uspxAmount / item.mwhAmount != tmp) {
+              count = index + 1;
+              tmp = item.uspxAmount / item.mwhAmount;
+            }
+            return { id: item.id, rank: count, name: item.name, mwhAmount: item.mwhAmount, uspxAmount: item.uspxAmount };
+          });
+        return sortedCo2Ranking;
+      }),
+    );
+    this.co2Rank$ = combineLatest([co2Rankings$, studentAccount$]).pipe(
+      map(([rankings, account]) => {
+        const ranking = rankings.find((ranking) => ranking.id == account.id);
+        return { rank: ranking?.rank!, uspxPercentage: ranking?.uspxAmount! / ranking?.mwhAmount! };
+      }),
+    );
+
+    // 8.This Month's Usage
+    this.totalUsage$ = usageListDaily$.pipe(
+      map((usages) => {
+        let count = 0;
+        for (const usage of usages) {
+          (usage.created_at as Timestamp).toDate() > firstDay ? (count += parseInt(usage.amount_kwh_str)) : count;
+        }
+        return count;
+      }),
+    );
+
+    this.totalUsageAverage$ = usageListDailyTotal$.pipe(
+      map((usages) => {
+        let count = 0;
+        for (const usage of usages) {
+          (usage.created_at as Timestamp).toDate() > firstDay ? (count += parseInt(usage.amount_kwh_str)) : count;
+        }
+        // higashi-20, koushi-26, sentetsu-28
+        return (count / (20 + 26 + 28)).toFixed(2);
+      }),
+    );
 
     const latestMonthlyUsage = usageListMonthly$.pipe(
       map(
@@ -535,6 +505,7 @@ export class DashboardComponent implements OnInit {
       }),
     );
 
+    //  9.LastMonth's Usage & CO2 Emission
     const lastMonthPayments$ = users$.pipe(
       mergeMap(async (users) => {
         let payments = [];
