63641ee70067cb5c69422dd3f8113a8e5e337714
Senna46 <29295263+Senna46@users.noreply.github.com>
2022-06-24 14:15:46 +0900

    fix: controller

--- functions/src/single-price-normal-settlements/controller.ts
+++ functions/src/single-price-normal-settlements/controller.ts
@@ -7,7 +7,7 @@ export const onCreateHandler: FirestoreCreateHandler[] = [];
 export const onUpdateHandler: FirestoreUpdateHandler[] = [];
 export const onDeleteHandler: FirestoreDeleteHandler[] = [];
 
-const f = functions.region('asia-northeast1');
+const f = functions.region('asia-northeast1').runWith({ timeoutSeconds: 540, memory: '2GB' });
 module.exports.onCreate = f.firestore.document(SinglePriceNormalSettlementFirestore.virtualPath).onCreate(async (snapshot, context) => {
   if (await isTriggeredOnce(context.eventId)) {
     return;
