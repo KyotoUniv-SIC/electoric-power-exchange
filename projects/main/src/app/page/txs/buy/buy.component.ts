e31a564220981d11a3e1e3bf5174f7c100bf3730
Senna Ohmura <29295263+Senna46@users.noreply.github.com>
2022-07-12 19:29:07 +0900

    feat: グラフ作成（normal）

--- projects/main/src/app/page/txs/buy/buy.component.ts
+++ projects/main/src/app/page/txs/buy/buy.component.ts
@@ -3,7 +3,13 @@ import { Component, OnInit } from '@angular/core';
 import { Auth, authState } from '@angular/fire/auth';
 import { Timestamp } from '@angular/fire/firestore';
 import { NormalBid, RenewableBid, SinglePriceNormalSettlement, SinglePriceRenewableSettlement, StudentAccount } from '@local/common';
+import { ChartDataSets } from 'chart.js';
+import { Label } from 'ng2-charts';
+import { NormalAskHistoryApplicationService } from 'projects/shared/src/lib/services/normal-ask-histories/normal-ask-history.application.service';
+import { NormalBidHistoryApplicationService } from 'projects/shared/src/lib/services/normal-bid-histories/normal-bid-history.application.service';
 import { NormalBidApplicationService } from 'projects/shared/src/lib/services/normal-bids/normal-bid.application.service';
+import { RenewableAskHistoryApplicationService } from 'projects/shared/src/lib/services/renewable-ask-histories/renewable-ask-history.application.service';
+import { RenewableBidHistoryApplicationService } from 'projects/shared/src/lib/services/renewable-bid-histories/renewable-bid-history.application.service';
 import { RenewableBidApplicationService } from 'projects/shared/src/lib/services/renewable-bids/renewable-bid.application.service';
 import { SinglePriceNormalSettlementApplicationService } from 'projects/shared/src/lib/services/single-price-normal-settlements/single-price-normal-settlement.application.service';
 import { SinglePriceRenewableSettlementApplicationService } from 'projects/shared/src/lib/services/single-price-renewable-settlements/single-price-renewable-settlement.application.service';
@@ -27,6 +33,8 @@ export class BuyComponent implements OnInit {
   singlePriceNormalDate$: Observable<Date> | undefined;
   singlePriceRenewable$: Observable<SinglePriceRenewableSettlement> | undefined;
   singlePriceRenewableDate$: Observable<Date> | undefined;
+  normalGraphPrices$: Observable<Label[]>;
+  normalGraphAmounts$: Observable<ChartDataSets[]>;
   price: number | undefined;
   amount: number | undefined;
   denom: string | undefined;
@@ -40,6 +48,10 @@ export class BuyComponent implements OnInit {
     private readonly insufficientBalanceApp: InsufficientBalanceApplicationService,
     private readonly singlePriceNormalApp: SinglePriceNormalSettlementApplicationService,
     private readonly singlePriceRenewableApp: SinglePriceRenewableSettlementApplicationService,
+    private readonly normalBidHistoryApp: NormalBidHistoryApplicationService,
+    private readonly normalAskHistoryApp: NormalAskHistoryApplicationService,
+    private readonly renewableBidHistoryApp: RenewableBidHistoryApplicationService,
+    private readonly renewableAskHistoryApp: RenewableAskHistoryApplicationService,
   ) {
     this.price = 27;
     this.amount = 1;
@@ -83,6 +95,119 @@ export class BuyComponent implements OnInit {
     this.singlePriceNormalDate$ = this.singlePriceNormal$.pipe(map((single) => (single.market_date as Timestamp).toDate()));
     this.singlePriceRenewable$ = this.singlePriceRenewableApp.getLatest$();
     this.singlePriceRenewableDate$ = this.singlePriceRenewable$.pipe(map((single) => (single.market_date as Timestamp).toDate()));
+
+    // 昨日分のグラフ作成
+    const historyNormalBids$ = this.normalBidHistoryApp.listYesterdayAll$();
+    const historyNormalAsks$ = this.normalAskHistoryApp.listYesterdayAll$();
+    const combines$ = combineLatest([historyNormalBids$, historyNormalAsks$]);
+    const maxPrice$ = combines$.pipe(
+      map(([bids, asks]) => {
+        const maxBid = bids.map((param) => parseInt(param.price_ujpy)).reduce((prev, current) => Math.max(prev, current));
+        const maxAsk = asks.map((param) => parseInt(param.price_ujpy)).reduce((prev, current) => Math.max(prev, current));
+        return Math.max(maxBid, maxAsk);
+      }),
+    );
+    const minPrice$ = combines$.pipe(
+      map(([bids, asks]) => {
+        const minBid = bids.map((param) => parseInt(param.price_ujpy)).reduce((prev, current) => Math.min(prev, current));
+        const minAsk = asks.map((param) => parseInt(param.price_ujpy)).reduce((prev, current) => Math.min(prev, current));
+        return Math.min(minBid, minAsk);
+      }),
+    );
+
+    this.normalGraphPrices$ = combineLatest([maxPrice$, minPrice$]).pipe(
+      map(([max, min]) => {
+        const minStr = (min / 1000000).toString();
+        const maxStr = (max / 1000000).toString();
+        const minCeil = Math.ceil(min / 100000);
+        const minCeilStr = (minCeil / 10).toString();
+        const maxFloor = Math.floor(max / 100000);
+        const maxFloorStr = (maxFloor / 10).toString();
+        if (minCeil == maxFloor) {
+          if (min == max) {
+            return [maxStr];
+          } else if (min == minCeil || max == minCeil) {
+            return [minStr, maxStr];
+          } else {
+            return [minStr, minCeilStr, maxStr];
+          }
+        } else {
+          let prices = [minStr];
+          if (minStr != minCeilStr) {
+            prices.push(minCeilStr);
+          }
+          let price = minCeil;
+          while (price < maxFloor) {
+            price++;
+            prices.push((price / 10).toString());
+          }
+          prices.push(maxFloorStr);
+
+          return prices;
+        }
+      }),
+    );
+
+    const NormalGraphBids$ = combineLatest([this.normalGraphPrices$, historyNormalBids$]).pipe(
+      map(([prices, bids]) =>
+        prices.map((price) => {
+          const ujpyPrice = Number(price) * 1000000;
+          if (Number.isInteger(ujpyPrice / 100000)) {
+            return bids.reduce((sum, current) => {
+              const currentPrice = parseInt(current.price_ujpy);
+              if (currentPrice >= ujpyPrice && currentPrice < ujpyPrice + 100000) {
+                return sum - parseInt(current.amount_uupx);
+              } else {
+                return sum;
+              }
+            }, 0);
+          } else {
+            return bids.reduce((sum, current) => {
+              const currentPrice = parseInt(current.price_ujpy);
+              if (currentPrice == ujpyPrice) {
+                return sum - parseInt(current.amount_uupx);
+              } else {
+                return sum;
+              }
+            }, 0);
+          }
+        }),
+      ),
+    );
+
+    const NormalGraphAsks$ = combineLatest([this.normalGraphPrices$, historyNormalAsks$]).pipe(
+      map(([prices, asks]) =>
+        prices.map((price) => {
+          const ujpyPrice = Number(price) * 1000000;
+          if (Number.isInteger(ujpyPrice / 100000)) {
+            return asks.reduce((sum, current) => {
+              const currentPrice = parseInt(current.price_ujpy);
+              if (currentPrice >= ujpyPrice && currentPrice < ujpyPrice + 100000) {
+                return sum + parseInt(current.amount_uupx);
+              } else {
+                return sum;
+              }
+            }, 0);
+          } else {
+            return asks.reduce((sum, current) => {
+              const currentPrice = parseInt(current.price_ujpy);
+              if (currentPrice == ujpyPrice) {
+                return sum + parseInt(current.amount_uupx);
+              } else {
+                return sum;
+              }
+            }, 0);
+          }
+        }),
+      ),
+    );
+
+    this.normalGraphAmounts$ = combineLatest([NormalGraphBids$, NormalGraphAsks$]).pipe(
+      map(([bids, asks]) => [
+        { data: bids, label: 'UPX Bids' },
+        { data: asks, label: 'UPX Asks' },
+      ]),
+    );
   }
 
   ngOnInit(): void {}
